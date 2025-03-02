3472ffa1ce4dff532c50628b709e0369c14fd3d5
tracer-ysyx <tracer@ysyx.org>
2025-03-02 09:17:58 -0500

    > sim RTL ysyx_22 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:17:58 up  1:15,  1 user,  load average: 0.70, 0.21, 0.08

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -4,10 +4,11 @@
 #include<Vtop.h>
 #include<verilated.h>
 
-Verilated::traceEverOn(true);
 
 int main()
 {
+
+    Verilated::traceEverOn(true);
     VerilatedVcdC *tfp = new VerilatedVcdC;
     top->trace(tfp, 99); // 99 是跟踪深度
     tfp->open("top.fst");
@@ -24,4 +25,9 @@ int main()
         printf("a=%d,b=%d,f=%d\n", top->a, top->b, top->f);
         assert(top->f == a ^ b);
     }
+
+    tfp->close();
+    delete top;
+    delete tfp;
+    return 0;
 }
