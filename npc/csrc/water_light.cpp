b05756833eb99ed8f18d17574aa45cd68bb355e3
tracer-ysyx <tracer@ysyx.org>
2025-03-04 09:50:13 -0500

    > sim RTL ysyx_22 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:50:13 up 32 min,  1 user,  load average: 0.05, 0.03, 0.05

--- npc/csrc/water_light.cpp
+++ npc/csrc/water_light.cpp
@@ -1,10 +1,8 @@
-
-#include<assert.h>
-#include<stdio.h>
-#include<Vtop.h>
-#include<verilated.h>
-#include<verilated_fst_c.h>
-
+#include <assert.h>
+#include <stdio.h>
+#include <Vtop.h>
+#include <verilated.h>
+#include <verilated_fst_c.h>
 
 Vtop *top = new Vtop;
 VerilatedFstC *tfp = new VerilatedFstC;
@@ -15,40 +13,48 @@ void single_cycle()
     top->clk = 0;
     top->eval();
     tfp->dump(main_time);
+    tfp->flush();
     main_time++;
     printf("reg=%d\n", top->led);
     top->clk = 1;
     top->eval();
     tfp->dump(main_time);
+    tfp->flush(); 
     main_time++;
     printf("reg=%d\n", top->led);
 }
+
 void reset(int n)
 {
     top->rst = 1;
-    while(n-- >0) single_cycle();
+    while (n-- > 0)
+        single_cycle();
     top->rst = 0;
-
 }
 
-int main(int argc,char** argv)
+int main(int argc, char **argv)
 {
     Verilated::commandArgs(argc, argv);
     Verilated::traceEverOn(true);
-   
+
     top->trace(tfp, 99);
     tfp->open("top.fst");
-   
+
+    if (!tfp->isOpen())
+    {
+        fprintf(stderr, "Failed to open trace file\n");
+        return -1;
+    }
 
     reset(10);
-
-    while(1)
+  
+    for (int i = 0; i < 100; i++)
     {
         single_cycle();
-      
     }
-    tfp->close();
 
+    tfp->close();
     delete top;
+    delete tfp; 
     return 0;
 }
