55fa3b0388057151ecb3e89a3266dd9d9192f8ca
tracer-ysyx <tracer@ysyx.org>
2025-03-08 08:10:47 -0500

    > sim RTL ysyx_22 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:10:47 up 22:27,  1 user,  load average: 0.08, 0.06, 0.01

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,14 +1,21 @@
 module top(
     input SW0,SW1,SW2,SW3,SW4,SW5,SW6,SW7,SW8,SW9,
-    output LD1,LD0
+    output LD1,LD0,
+    output reg [1:0] Y,
+    output reg [1:0] X0,
+    output reg [1:0] X1,
+    output reg [1:0] X2,
+    output reg [1:0] X3,
+    output reg [1:0] F
 );
+always @(*) begin
 
-reg [1:0] Y={SW1,SW0};
-reg [1:0] X0={SW3,SW2};
-reg [1:0] X1={SW5,SW4};
-reg [1:0] X2={SW7,SW6};
-reg [1:0] X3={SW9,SW8};
-reg [1:0] F;
+    Y={SW1,SW0};
+    X0={SW3,SW2};
+    X1={SW5,SW4};
+    X2={SW7,SW6};
+    X3={SW9,SW8};
+end
 
 mu241 mld(.Y(Y),.X0(X0),.X1(X1),.X2(X2),.X3(X3),.F(F));
 
