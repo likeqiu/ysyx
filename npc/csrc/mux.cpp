d486b276e9f9913a1a03f62006c814cfd419ffab
tracer-ysyx <tracer@ysyx.org>
2025-03-08 10:01:13 -0500

    > sim RTL ysyx_22 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:01:13 up  1:33,  1 user,  load average: 0.04, 0.05, 0.00

--- npc/csrc/mux.cpp
+++ npc/csrc/mux.cpp
@@ -5,13 +5,7 @@ static Vtop dut;
 
 void nvboard_bind_all_pins(Vtop *top);
 
-static void single_cycle()
-{
-    dut.clk = 0;
-    dut.eval();
-    dut.clk = 1;
-    dut.eval();
-}
+
 int main()
 {
     nvboard_bind_all_pins(&dut);
