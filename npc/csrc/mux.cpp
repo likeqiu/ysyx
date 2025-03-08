48c0cf31459d33983499c207b8699640ec4cde67
tracer-ysyx <tracer@ysyx.org>
2025-03-08 10:49:46 -0500

    > sim RTL ysyx_22 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:49:46 up  2:21,  1 user,  load average: 0.08, 0.10, 0.05

--- npc/csrc/mux.cpp
+++ npc/csrc/mux.cpp
@@ -1,42 +1,27 @@
-#include <stdio.h>
-#include <unistd.h>
 #include<Vtop.h>
 #include<nvboard.h>
 
 static Vtop dut;
+
 void nvboard_bind_all_pins(Vtop *top);
+
 
 int main()
 {
     nvboard_bind_all_pins(&dut);
     nvboard_init();
-
-    int count = 0;
-
     while (1)
-   
-        dut.eval();
-
-        if (count % 100 == 0)
-        {
-            printf("Y=%d, F=%d, LD1=%d, LD0=%d\n",
-                   (int)dut.Y, (int)dut.F, (int)dut.LD1, (int)dut.LD0);
-        }
+    {
+        
+        dut.eval();   
+        usleep(5000);
+        dut.eval(); 
 
         nvboard_update();
-
-
-        if (count % 1000 < 10)
-        {
-            usleep(50); 
-        }
-        else
-        {
-            usleep(5); 
-
-        count++;
+        usleep(10);     
     }
-    nvboard_quit();
 
     return 0;
 }
+
+
