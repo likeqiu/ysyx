9da5f08601ca090088303b0cf515d077869414d3
tracer-ysyx <tracer@ysyx.org>
2025-03-08 05:01:32 -0500

    > sim RTL ysyx_22 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:01:32 up 19:17,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,11 +1,11 @@
 module top(
-    output reg [1:0] out,
-    input reg [1:0] key,
+    output reg [1:0] F,
+    input reg [1:0] Y,
     input  reg [1:0] X0,X1,X2,X3
 );
 
 
-MuxKey #(4,2,2) i0 (out,key,{
+MuxKey #(4,2,2) i0 (F,Y,{
     2'b00,X0[1:0],
     2'b01,X1[1:0],
     2'b10,X2[1:0],
