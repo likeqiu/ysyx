29aabb062bb2305cae54ab48ecde581487facacf
tracer-ysyx <tracer@ysyx.org>
2025-03-09 00:57:42 -0500

    > sim RTL ysyx_22 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:57:42 up  7:44,  1 user,  load average: 0.06, 0.08, 0.03

--- npc/csrc/encorder.cpp
+++ npc/csrc/encorder.cpp
@@ -1,9 +1,9 @@
-#include<Vtop.h>
-#include<nvboard.h>
+#include <Vtop.h>
+#include <nvboard.h>
 
 static Vtop dut;
 
-void nvboard_bind_pins_all(Vtop *top)
+void nvboard_bind_all_pins(Vtop *top);
 
 static void single_cycle()
 {
@@ -15,15 +15,13 @@ static void single_cycle()
 
 int main()
 {
-    nvboard_bind_pins_all(&dut);
+    nvboard_bind_all_pins(&dut);
     nvboard_init();
-
     while (1)
     {
         nvboard_update();
         single_cycle();
-       
     }
-    nvboard_quit();
+
     return 0;
 }
