524450d3b12e830fe6e21a673994d822abeac9c2
tracer-ysyx <tracer@ysyx.org>
2025-03-08 20:44:00 -0500

    > sim RTL ysyx_22 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:44:00 up  3:32,  1 user,  load average: 0.08, 0.08, 0.04

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -23,10 +23,10 @@ MuxKey #(4,2,2) i0 (
 );
 
 always @(posedge clk)begin
-    if(change==X0) F=2'b0;
-    if(change==X1) F=2'b1;
-    if(change==X2) F=2'b10;
-    if(change==X3) F=2'b11;
+    if(change==X0 & Y==2'b00) F=2'b0;
+    if(change==X1 & Y==2'b01) F=2'b1;
+    if(change==X2 & Y==2'b10) F=2'b10;
+    if(change==X3 & Y==2'b11) F=2'b11;
 end
 
 
