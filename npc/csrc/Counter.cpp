aca076b67a030e3339038a912064ef022d811a9e
tracer-ysyx <tracer@ysyx.org>
2025-03-15 12:28:28 -0400

    > sim RTL ysyx_22 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:28:28 up 1 day,  4:12,  1 user,  load average: 0.42, 0.42, 0.40

--- npc/csrc/Counter.cpp
+++ npc/csrc/Counter.cpp
@@ -7,9 +7,9 @@ void nvboard_bind_all_pins(VCounterModule *top);
 
 static void single_cycle()
 {
-    dut.clk = 0;
+    dut.clock = 0;
     dut.eval();
-    dut.clk = 1;
+    dut.clock = 1;
     dut.eval();
 }
 
