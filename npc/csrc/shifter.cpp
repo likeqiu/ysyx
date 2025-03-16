a10cb6f356582be69ed8dd375561bf3f75ab9cb0
tracer-ysyx <tracer@ysyx.org>
2025-03-16 01:36:46 -0400

    > sim RTL ysyx_22 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:36:46 up 1 day,  9:46,  1 user,  load average: 0.09, 0.14, 0.14

--- npc/csrc/shifter.cpp
+++ npc/csrc/shifter.cpp
@@ -7,33 +7,22 @@ void nvboard_bind_all_pins(Vtop *top);
 
 static void single_cycle()
 {
-    dut.clk = 0;
+    dut.clock = 0;
     dut.eval();
-    dut.clk = 1;
+    dut.clock = 1;
     dut.eval();
-    
+    usleep(1);
 }
 
 int main()
 {
     nvboard_bind_all_pins(&dut);
     nvboard_init();
-
-    int count = 0;
-
     while (1)
-    { 
-        if(count==50){
-            single_cycle();
-            count = 0;
-        }
-        else
-        {
-            
-            count++;
-        }
+    {
         nvboard_update();
+        single_cycle()
+    }
 
-        return 0;
-}
+    return 0;
 }
