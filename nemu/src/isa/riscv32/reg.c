f299b731e0ac28d0a7b0c9c74a6037e4e55c3a9e
tracer-ysyx <tracer@ysyx.org>
2025-03-22 12:00:36 -0400

    > compile NEMU ysyx_22 zxj Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:00:36 up 3 days,  3:08,  1 user,  load average: 0.28, 0.16, 0.16

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,7 +25,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
   for (int i = 0; i < 32;i++){
-    printf("%s = 0x%x , ", regs[i], gpr(i));
+    printf("%s = 0x%x\n", regs[i], gpr(i));
   }
 }
 
