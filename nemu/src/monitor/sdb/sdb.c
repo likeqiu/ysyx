304be95dfb5048c329a38adbe62f0f836e9caf01
tracer-ysyx <tracer@ysyx.org>
2025-03-27 21:28:41 -0400

    > compile NEMU ysyx_22 zxj Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:28:41 up 4 days,  7:43,  1 user,  load average: 0.52, 0.24, 0.18

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -79,7 +79,9 @@ static int cmd_scan_pmem(char *args)
   
   sscanf(args, "%d", &n);
   char *get_vadd = strtok(args," ");
+  printf("%s", get_vadd);
   get_vadd = strtok(NULL, " ");
+  printf("%s", get_vadd);
   vadd = expr(get_vadd, &success); // 大于2*10^10+1*10^9+5*10^8
                                    // 小于2*10^10+2*10^9+7*10^8
   for (int i = 0; i < n;i++){
