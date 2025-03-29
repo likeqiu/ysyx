580bfc0041b7c01a83ac65d2b01927a60b8e8166
tracer-ysyx <tracer@ysyx.org>
2025-03-28 22:46:23 -0400

    > compile NEMU ysyx_22 zxj Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:46:23 up 4 days, 15:43,  1 user,  load average: 0.15, 0.15, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -103,7 +103,7 @@ static bool make_token(char *e) {
         if (nr_token < 65536)
         {
           tokens[nr_token].type = rules[i].token_type;
-          if (substr_len < 32)
+          if (substr_len < 65536)
           {
             strncpy(tokens[nr_token].str, substr_start, substr_len);
             tokens[nr_token].str[substr_len] = '\0';
