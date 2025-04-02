e55d684fb3fa740d07ac26eff164d25ac9b12913
tracer-ysyx <tracer@ysyx.org>
2025-04-02 10:05:43 -0400

    > compile NEMU ysyx_22 zxj Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:05:43 up 5 days, 21:18,  1 user,  load average: 0.04, 0.11, 0.17

---
+++ npc/obj_dir/Vtop___024root.h
@@ -0,0 +1,50 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design internal header
+// See Vtop.h for the primary calling header
+
+#ifndef VERILATED_VTOP___024ROOT_H_
+#define VERILATED_VTOP___024ROOT_H_  // guard
+
+#include "verilated.h"
+
+class Vtop__Syms;
+
+class Vtop___024root final : public VerilatedModule {
+  public:
+
+    // DESIGN SPECIFIC STATE
+    VL_IN8(clk,0,0);
+    VL_IN8(reset,0,0);
+    VL_OUT8(seg0,6,0);
+    VL_OUT8(seg1,6,0);
+    VL_IN8(num,7,0);
+    VL_OUT8(led,0,0);
+    CData/*7:0*/ top__DOT__temp_shift;
+    CData/*0:0*/ top__DOT__random;
+    CData/*0:0*/ __Vtrigrprev__TOP__clk;
+    CData/*0:0*/ __Vtrigrprev__TOP__reset;
+    CData/*0:0*/ __VactContinue;
+    IData/*25:0*/ top__DOT__count;
+    IData/*31:0*/ __VstlIterCount;
+    IData/*31:0*/ __VicoIterCount;
+    IData/*31:0*/ __VactIterCount;
+    VlUnpacked<CData/*0:0*/, 2> __Vm_traceActivity;
+    VlTriggerVec<1> __VstlTriggered;
+    VlTriggerVec<1> __VicoTriggered;
+    VlTriggerVec<1> __VactTriggered;
+    VlTriggerVec<1> __VnbaTriggered;
+
+    // INTERNAL VARIABLES
+    Vtop__Syms* const vlSymsp;
+
+    // CONSTRUCTORS
+    Vtop___024root(Vtop__Syms* symsp, const char* v__name);
+    ~Vtop___024root();
+    VL_UNCOPYABLE(Vtop___024root);
+
+    // INTERNAL METHODS
+    void __Vconfigure(bool first);
+} VL_ATTR_ALIGNED(VL_CACHE_LINE_BYTES);
+
+
+#endif  // guard
---
+++ npc/obj_dir/Vtop___024root__DepSet_heccd7ead__0__Slow.cpp
@@ -0,0 +1,174 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vtop.h for the primary calling header
+
+#include "verilated.h"
+
+#include "Vtop___024root.h"
+
+VL_ATTR_COLD void Vtop___024root___eval_static(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_static\n"); );
+}
+
+VL_ATTR_COLD void Vtop___024root___eval_initial(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_initial\n"); );
+    // Body
+    vlSelf->__Vtrigrprev__TOP__clk = vlSelf->clk;
+    vlSelf->__Vtrigrprev__TOP__reset = vlSelf->reset;
+}
+
+VL_ATTR_COLD void Vtop___024root___eval_final(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_final\n"); );
+}
+
+VL_ATTR_COLD void Vtop___024root___eval_triggers__stl(Vtop___024root* vlSelf);
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vtop___024root___dump_triggers__stl(Vtop___024root* vlSelf);
+#endif  // VL_DEBUG
+VL_ATTR_COLD void Vtop___024root___eval_stl(Vtop___024root* vlSelf);
+
+VL_ATTR_COLD void Vtop___024root___eval_settle(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_settle\n"); );
+    // Init
+    CData/*0:0*/ __VstlContinue;
+    // Body
+    vlSelf->__VstlIterCount = 0U;
+    __VstlContinue = 1U;
+    while (__VstlContinue) {
+        __VstlContinue = 0U;
+        Vtop___024root___eval_triggers__stl(vlSelf);
+        if (vlSelf->__VstlTriggered.any()) {
+            __VstlContinue = 1U;
+            if (VL_UNLIKELY((0x64U < vlSelf->__VstlIterCount))) {
+#ifdef VL_DEBUG
+                Vtop___024root___dump_triggers__stl(vlSelf);
+#endif
+                VL_FATAL_MT("vsrc/shifter.v", 3, "", "Settle region did not converge.");
+            }
+            vlSelf->__VstlIterCount = ((IData)(1U) 
+                                       + vlSelf->__VstlIterCount);
+            Vtop___024root___eval_stl(vlSelf);
+        }
+    }
+}
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vtop___024root___dump_triggers__stl(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___dump_triggers__stl\n"); );
+    // Body
+    if ((1U & (~ (IData)(vlSelf->__VstlTriggered.any())))) {
+        VL_DBG_MSGF("         No triggers active\n");
+    }
+    if (vlSelf->__VstlTriggered.at(0U)) {
+        VL_DBG_MSGF("         'stl' region trigger index 0 is active: Internal 'stl' trigger - first iteration\n");
+    }
+}
+#endif  // VL_DEBUG
+
+extern const VlUnpacked<CData/*6:0*/, 16> Vtop__ConstPool__TABLE_hea8de336_0;
+
+VL_ATTR_COLD void Vtop___024root___stl_sequent__TOP__0(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___stl_sequent__TOP__0\n"); );
+    // Init
+    CData/*3:0*/ __Vtableidx1;
+    __Vtableidx1 = 0;
+    CData/*3:0*/ __Vtableidx2;
+    __Vtableidx2 = 0;
+    // Body
+    vlSelf->led = vlSelf->reset;
+    __Vtableidx1 = (0xfU & (IData)(vlSelf->top__DOT__temp_shift));
+    vlSelf->seg0 = Vtop__ConstPool__TABLE_hea8de336_0
+        [__Vtableidx1];
+    __Vtableidx2 = (0xfU & ((IData)(vlSelf->top__DOT__temp_shift) 
+                            >> 4U));
+    vlSelf->seg1 = Vtop__ConstPool__TABLE_hea8de336_0
+        [__Vtableidx2];
+}
+
+VL_ATTR_COLD void Vtop___024root___eval_stl(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_stl\n"); );
+    // Body
+    if (vlSelf->__VstlTriggered.at(0U)) {
+        Vtop___024root___stl_sequent__TOP__0(vlSelf);
+    }
+}
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vtop___024root___dump_triggers__ico(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___dump_triggers__ico\n"); );
+    // Body
+    if ((1U & (~ (IData)(vlSelf->__VicoTriggered.any())))) {
+        VL_DBG_MSGF("         No triggers active\n");
+    }
+    if (vlSelf->__VicoTriggered.at(0U)) {
+        VL_DBG_MSGF("         'ico' region trigger index 0 is active: Internal 'ico' trigger - first iteration\n");
+    }
+}
+#endif  // VL_DEBUG
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vtop___024root___dump_triggers__act(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___dump_triggers__act\n"); );
+    // Body
+    if ((1U & (~ (IData)(vlSelf->__VactTriggered.any())))) {
+        VL_DBG_MSGF("         No triggers active\n");
+    }
+    if (vlSelf->__VactTriggered.at(0U)) {
+        VL_DBG_MSGF("         'act' region trigger index 0 is active: @(posedge clk or posedge reset)\n");
+    }
+}
+#endif  // VL_DEBUG
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vtop___024root___dump_triggers__nba(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___dump_triggers__nba\n"); );
+    // Body
+    if ((1U & (~ (IData)(vlSelf->__VnbaTriggered.any())))) {
+        VL_DBG_MSGF("         No triggers active\n");
+    }
+    if (vlSelf->__VnbaTriggered.at(0U)) {
+        VL_DBG_MSGF("         'nba' region trigger index 0 is active: @(posedge clk or posedge reset)\n");
+    }
+}
+#endif  // VL_DEBUG
+
+VL_ATTR_COLD void Vtop___024root___ctor_var_reset(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___ctor_var_reset\n"); );
+    // Body
+    vlSelf->seg0 = VL_RAND_RESET_I(7);
+    vlSelf->seg1 = VL_RAND_RESET_I(7);
+    vlSelf->num = VL_RAND_RESET_I(8);
+    vlSelf->clk = VL_RAND_RESET_I(1);
+    vlSelf->reset = VL_RAND_RESET_I(1);
+    vlSelf->led = VL_RAND_RESET_I(1);
+    vlSelf->top__DOT__temp_shift = VL_RAND_RESET_I(8);
+    vlSelf->top__DOT__random = VL_RAND_RESET_I(1);
+    vlSelf->top__DOT__count = VL_RAND_RESET_I(26);
+    vlSelf->__Vtrigrprev__TOP__clk = VL_RAND_RESET_I(1);
+    vlSelf->__Vtrigrprev__TOP__reset = VL_RAND_RESET_I(1);
+    for (int __Vi0 = 0; __Vi0 < 2; ++__Vi0) {
+        vlSelf->__Vm_traceActivity[__Vi0] = 0;
+    }
+}
