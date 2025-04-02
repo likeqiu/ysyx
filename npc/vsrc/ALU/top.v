e9751eb03d5791f5c0cc31b0e761d1d497773e96
tracer-ysyx <tracer@ysyx.org>
2025-04-02 08:59:24 -0400

    > sim RTL ysyx_22 zxj Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:59:24 up 5 days, 20:12,  1 user,  load average: 0.14, 0.31, 0.50

--- npc/vsrc/ALU/top.v
+++ npc/vsrc/ALU/top.v
@@ -13,11 +13,11 @@ module top(
 
 ALU test(.clk(clk),.select(select),.a(a),.b(b),.result(result),.zero(zero),.cin(cin),.overflow(overflow),.compare_out(compare_out),.seg0(seg0),.seg1(seg1));    
 
-always @(result) begin
+/*always @(result) begin
     $display("a=%d, b=%d ,result=%d,zero=%d,cin=%d,overflow=%d,compare_out=%d,",a,b,result,zero,cin,overflow,compare_out);
-end
+end*/
 
 //成功实现功能，但数码管我只设置了0~13
-//引脚对应select (BTNL,BTNC,BTNR) ，zero LD0，compare_out LD1，cin LD2，overflow LD3 
+//引脚对应select(BTNL,BTNC,BTNR) ，zero LD0，compare_out LD1，cin LD2，overflow LD3 
 
 endmodule
