1228a985aaedbab35891c67a035f602285647e30
tracer-ysyx <tracer@ysyx.org>
2025-04-02 08:05:38 -0400

    > sim RTL ysyx_22 zxj Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:05:38 up 5 days, 19:23,  1 user,  load average: 0.02, 0.28, 0.27

---
+++ npc/csrc/Counter.cpp
@@ -0,0 +1,28 @@
+#include <VCounterModule.h>
+#include <nvboard.h>
+
+static VCounterModule dut;
+
+void nvboard_bind_all_pins(VCounterModule *top);
+
+static void single_cycle()
+{
+    dut.clock = 0;
+    dut.eval();
+    dut.clock = 1;
+    dut.eval();
+    usleep(1);
+}
+
+int main()
+{
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+    while (1)
+    {
+        nvboard_update();
+        single_cycle()
+    }
+
+    return 0;
+}
---
+++ npc/csrc/old/mux.cpp
@@ -0,0 +1,29 @@
+#include<Vtop.h>
+#include<nvboard.h>
+
+static Vtop dut;
+
+void nvboard_bind_all_pins(Vtop *top);
+
+static void single_cycle()
+{
+    dut.clk = 0;
+    dut.eval();
+    dut.clk = 1;
+    dut.eval();
+}
+
+int main()
+{
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+    while (1)
+    {
+        nvboard_update();
+        single_cycle();
+    }
+
+    return 0;
+}
+
+
