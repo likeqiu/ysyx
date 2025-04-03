e5419d1cdb3eca5a77b9e43ec221a77d7a0cbdee
tracer-ysyx <tracer@ysyx.org>
2025-04-03 07:13:44 -0400

    > compile NEMU ysyx_22 zxj Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:13:44 up 5 days, 22:05,  1 user,  load average: 0.32, 0.33, 0.27

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -35,8 +35,8 @@ word_t isa_reg_str2val(const char *s, bool *success)
   if(strcmp("pc",s)==0)
   {
     *success = true;
-    word_t pc = cpu.pc;
-    return pc;
+    
+    return (unsigned int)cpu.pc;
   }
   else
   {
