0e15ef58fef83d07942d3daa7de4792c23106c65
tracer-ysyx <tracer@ysyx.org>
2025-04-04 11:32:56 -0400

    > compile NEMU ysyx_22 zxj Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:32:56 up 6 days, 14:44,  1 user,  load average: 0.30, 0.21, 0.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -47,7 +47,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   while(wp!=NULL)
   {
 
-    if (wp->old_value == cpu.pc && wp->type=='b')
+    if (wp->old_value == dnpc && wp->type=='b')
     {
       nemu_state.state = NEMU_STOP;
       printf("Hit an breakpoint NO:%-4d addr:0x%-5x\n", wp->NO, wp->old_value);
