00e57ccb28867a6e384d6e6ac3c8a0978166b481
tracer-ysyx <tracer@ysyx.org>
2025-04-04 02:38:30 -0400

    > sim RTL ysyx_22 zxj Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:38:30 up 6 days,  5:59,  1 user,  load average: 0.25, 0.14, 0.10

--- npc/obj_dir/Vtop___024root__DepSet_heccd7ead__0.cpp
+++ npc/obj_dir/Vtop___024root__DepSet_heccd7ead__0.cpp
@@ -23,33 +23,34 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) 
     __Vtableidx1 = 0;
     // Body
     vlSelf->compare_out = 0U;
-    if ((0U != (IData)(vlSelf->select))) {
-        if ((2U != (IData)(vlSelf->select))) {
-            if ((3U != (IData)(vlSelf->select))) {
-                if ((4U != (IData)(vlSelf->select))) {
-                    if ((5U != (IData)(vlSelf->select))) {
-                        if ((6U == (IData)(vlSelf->select))) {
-                            if (((IData)(vlSelf->a) 
-                                 > (IData)(vlSelf->b))) {
-                                vlSelf->compare_out = 1U;
-                            } else if (((IData)(vlSelf->a) 
-                                        < (IData)(vlSelf->b))) {
-                                vlSelf->compare_out = 0U;
-                            }
-                        } else if ((7U == (IData)(vlSelf->select))) {
-                            vlSelf->compare_out = ((IData)(vlSelf->a) 
-                                                   == (IData)(vlSelf->b));
-                        }
-                    }
-                }
-            }
-        }
-    }
     vlSelf->result = 0U;
     vlSelf->zero = 0U;
     vlSelf->cin = 0U;
     vlSelf->overflow = 0U;
-    if ((0U == (IData)(vlSelf->select))) {
+    if ((4U & (IData)(vlSelf->select))) {
+        if ((2U & (IData)(vlSelf->select))) {
+            if ((1U & (IData)(vlSelf->select))) {
+                vlSelf->compare_out = ((IData)(vlSelf->a) 
+                                       == (IData)(vlSelf->b));
+            } else if (((IData)(vlSelf->a) > (IData)(vlSelf->b))) {
+                vlSelf->compare_out = 1U;
+            } else if (((IData)(vlSelf->a) < (IData)(vlSelf->b))) {
+                vlSelf->compare_out = 0U;
+            }
+        }
+        if ((1U & (~ ((IData)(vlSelf->select) >> 1U)))) {
+            vlSelf->result = ((1U & (IData)(vlSelf->select))
+                               ? ((IData)(vlSelf->a) 
+                                  ^ (IData)(vlSelf->b))
+                               : ((IData)(vlSelf->a) 
+                                  | (IData)(vlSelf->b)));
+        }
+    } else if ((2U & (IData)(vlSelf->select))) {
+        vlSelf->result = (0xfU & ((1U & (IData)(vlSelf->select))
+                                   ? ((IData)(vlSelf->a) 
+                                      & (IData)(vlSelf->b))
+                                   : (~ (IData)(vlSelf->a))));
+    } else if ((1U & (~ (IData)(vlSelf->select)))) {
         vlSelf->result = (0xfU & ((IData)(vlSelf->a) 
                                   + (IData)(vlSelf->b)));
         if ((1U & ((~ ((IData)(vlSelf->a) >> 3U)) & 
@@ -68,14 +69,6 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) 
         }
         vlSelf->cin = vlSelf->overflow;
         vlSelf->zero = (0U == (IData)(vlSelf->result));
-    } else if ((2U == (IData)(vlSelf->select))) {
-        vlSelf->result = (0xfU & (~ (IData)(vlSelf->a)));
-    } else if ((3U == (IData)(vlSelf->select))) {
-        vlSelf->result = ((IData)(vlSelf->a) & (IData)(vlSelf->b));
-    } else if ((4U == (IData)(vlSelf->select))) {
-        vlSelf->result = ((IData)(vlSelf->a) | (IData)(vlSelf->b));
-    } else if ((5U == (IData)(vlSelf->select))) {
-        vlSelf->result = ((IData)(vlSelf->a) ^ (IData)(vlSelf->b));
     }
     __Vtableidx1 = vlSelf->result;
     vlSelf->seg0 = Vtop__ConstPool__TABLE_h13ff2797_0
