2d5bbeffb14fbd5019344fc8623e3b601becbfc1
tracer-ysyx <tracer@ysyx.org>
2025-04-04 02:49:36 -0400

    > sim RTL ysyx_22 zxj Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:49:36 up 6 days,  6:10,  1 user,  load average: 0.23, 0.16, 0.11

--- npc/auto_bind.cpp
+++ npc/auto_bind.cpp
@@ -2,14 +2,10 @@
 #include "Vtop.h"
 
 void nvboard_bind_all_pins(Vtop* top) {
-	nvboard_bind_pin( &top->ps2_clk, 1, PS2_CLK);
-	nvboard_bind_pin( &top->ps2_date, 1, PS2_DAT);
-	nvboard_bind_pin( &top->seg0, 7, SEG0A, SEG0B, SEG0C, SEG0D, SEG0E, SEG0F, SEG0G);
-	nvboard_bind_pin( &top->seg1, 7, SEG1A, SEG1B, SEG1C, SEG1D, SEG1E, SEG1F, SEG1G);
-	nvboard_bind_pin( &top->seg2, 7, SEG2A, SEG2B, SEG2C, SEG2D, SEG2E, SEG2F, SEG2G);
-	nvboard_bind_pin( &top->seg3, 7, SEG3A, SEG3B, SEG3C, SEG3D, SEG3E, SEG3F, SEG3G);
-	nvboard_bind_pin( &top->seg4, 7, SEG6A, SEG6B, SEG6C, SEG6D, SEG6E, SEG6F, SEG6G);
-	nvboard_bind_pin( &top->seg5, 7, SEG7A, SEG7B, SEG7C, SEG7D, SEG7E, SEG7F, SEG7G);
-	nvboard_bind_pin( &top->nextdate_n, 1, SW0);
-	nvboard_bind_pin( &top->clrk, 1, SW1);
+	nvboard_bind_pin( &top->Y, 2, SW1, SW0);
+	nvboard_bind_pin( &top->X0, 2, SW3, SW2);
+	nvboard_bind_pin( &top->X1, 2, SW5, SW4);
+	nvboard_bind_pin( &top->X2, 2, SW7, SW6);
+	nvboard_bind_pin( &top->X3, 2, SW9, SW8);
+	nvboard_bind_pin( &top->F, 2, LD1, LD0);
 }
