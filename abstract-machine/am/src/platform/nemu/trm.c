a5c274e108fda195ccfc5293a19660121299fd6c
tracer-ysyx <tracer@ysyx.org>
2025-04-18 22:26:30 -0400

    > compile NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:26:30 up 8 days,  5:11,  1 user,  load average: 1.07, 0.87, 0.67

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -1,24 +1,24 @@
 #include <am.h>
 #include <nemu.h>
 
-extern char _heap_start;
+extern char _heap_start;  //heap表示可用堆空间区域
 int main(const char *args);
 
-Area heap = RANGE(&_heap_start, PMEM_END);
-static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
+Area heap = RANGE(&_heap_start, PMEM_END);             // RANGE：宏或函数，构造一个内存区域，
+static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS ,由编译器替换为实际的参数
 
 void putch(char ch) {
-  outb(SERIAL_PORT, ch);
+  outb(SERIAL_PORT, ch);//SERIAL_PORT表示端口,putch输出一个字节
 }
 
 void halt(int code) {
-  nemu_trap(code);
+  nemu_trap(code);//停止运行，code为0程序正常结束
 
   // should not reach here
   while (1);
 }
 
 void _trm_init() {
-  int ret = main(mainargs);
+  int ret = main(mainargs);//启动用户程序
   halt(ret);
 }
