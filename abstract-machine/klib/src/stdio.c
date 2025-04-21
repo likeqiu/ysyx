a74b3baf06815b45ebd67f1ed8e569191cc91250
tracer-ysyx <tracer@ysyx.org>
2025-04-21 06:08:30 -0400

    > compile NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  06:08:30 up 8 days, 22:45,  1 user,  load average: 0.50, 0.44, 0.49

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,7 +5,10 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
+static int itoa(int num, char *buf);
+
+int printf(const char *fmt, ...)
+{
   panic("Not implemented");
 }
 
@@ -14,6 +17,50 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+  va_list args;
+  char *d = out;
+  int count = 0;
+
+  va_start(args, fmt);
+
+  while(*fmt)
+  {
+    if(*fmt != '%')
+    {
+      *d++ = *fmt++;
+      count++;
+    }else{
+      fmt++;
+      if(*fmt=='s')
+      {
+        char *s = va_arg(args, char *);//指向char*类型参数
+        while(*s)
+        {
+          *d++ = *s++;
+          count++;
+        }
+        fmt++;
+      }else if(*fmt=='d')
+      {
+        int num = va_arg(args, int);
+        char temp[32];
+        int len = itoa(num, temp);
+        for (int i = 0; i < len;i++)
+        {
+          *d++ = temp[i];
+          count++;
+        }
+        fmt++;
+      }else{
+        //还未支持的格式,直接写入"%
+        *d++ = '%';
+        count++;
+      }
+    }
+  }
+  *d = '\0';
+  va_end(args);
+  return count;
   panic("Not implemented");
 }
 
@@ -23,6 +70,41 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   panic("Not implemented");
+}
+
+static int itoa(int num,char *buf)
+{
+  char temp[32];
+  int len = 0, i = 0;
+  int is_negative = num < 0;
+
+  if(is_negative)
+  {
+    num = -num;
+  }
+
+  if(num==0)
+  {
+    temp[i++] = '0';
+  }
+
+  while(num > 0)
+  {
+    temp[i++] = '0' + (num % 10);
+    num /= 10;
+  }
+
+  if(is_negative)
+  {
+    temp[i++] = '-';
+  }
+
+  while(i>0)
+  {
+    buf[len++] = temp[--i];
+  }
+  buf[len] = '\0';
+  return len;
 }
 
 #endif
