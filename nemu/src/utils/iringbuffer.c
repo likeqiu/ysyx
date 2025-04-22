2833f3deb7384c39704118c5089d607067471c16
tracer-ysyx <tracer@ysyx.org>
2025-04-22 09:20:12 -0400

    > compile NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:20:12 up 9 days,  5:36,  1 user,  load average: 0.72, 0.72, 0.62

--- nemu/src/utils/iringbuffer.c
+++ nemu/src/utils/iringbuffer.c
@@ -1,7 +1,7 @@
 #include <common.h>
 
 
-#define MAX_BUFFER 16;
+#define IRINGBUF_SIZE 16
 
     typedef struct 
 {
@@ -9,3 +9,19 @@
     uint32_t inst;
     char disasm_str[64];
 } IRingBufEntry;
+
+static IRingBufEntry iringbuf[IRINGBUF_SIZE];
+static int iringbuf_index = 0;
+static bool iringbuf_full = false;
+
+void init_iringbuffer()
+{
+    iringbuf_index = 0;
+    iringbuf_full = false;
+    for (int i = 0;i < IRINGBUF_SIZE ; i++)
+    {
+        iringbuf[i].pc = 0;
+        iringbuf[i].inst = 0;
+        iringbuf[i].disasm_str[0] = '\0';
+    }
+}
