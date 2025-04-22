803eef88352cc45763a465a30424eddaedc2235b
tracer-ysyx <tracer@ysyx.org>
2025-04-22 08:54:16 -0400

    > compile NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:54:16 up 9 days,  5:11,  1 user,  load average: 0.45, 0.46, 0.45

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -25,7 +25,7 @@ static inline uint32_t inst_fetch(vaddr_t *pc, int len)
   vaddr_t orig_pc = *pc;
   (*pc) += len;
 
-  printf("inst_fetch: PC=0x%x, Inst=0x%08x, Len=%d\n", orig_pc, inst, len);
+
   char disasm_str[64];
   disassemble(disasm_str, sizeof(disasm_str), orig_pc, (uint8_t *)&inst, len);
   log_write("0x%x: %08x %s\n", orig_pc, inst, disasm_str);
