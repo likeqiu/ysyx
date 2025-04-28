d4402df54d8ec4987eafcf2a5e85f42296d37dee
tracer-ysyx <tracer@ysyx.org>
2025-04-28 07:11:05 -0400

    > compile NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:11:05 up 11:12,  1 user,  load average: 0.12, 0.06, 0.08

--- nemu/src/isa/riscv32/ftrace.c
+++ nemu/src/isa/riscv32/ftrace.c
@@ -1,31 +1,30 @@
-#include<elf.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<isa.h>
-#include<memory/paddr.h>
+#include <elf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <isa.h>
+#include <memory/paddr.h>
 #include <cpu/cpu.h>
 
-typedef struct 
+typedef struct
 {
     uint32_t name_offset;
     uint32_t value;
     uint32_t size;
-}FuncSymbol;
-//保存一个函数符号的信
+} FuncSymbol;
+// 保存一个函数符号的信
 
 static FuncSymbol *func_symbols = NULL; // 保存所有函数符号信息的数组
 static int func_count = 0;
 static char *strtab_data = NULL; // 字符串表的内容
-static uint32_t strtab_size = 0; 
-static int indent_level = 0;//调用层次
+static uint32_t strtab_size = 0;
+static int indent_level = 0; // 调用层次
 
-
-//提取符号表信息
+// 提取符号表信息
 void init_ftrace(const char *elf_file)
 {
     FILE *fp = fopen(elf_file, "rb");
-    if(!fp)
+    if (!fp)
     {
         printf("Error: Failed to open ELF file: %s \n", elf_file);
         exit(1);
@@ -40,7 +39,9 @@ void init_ftrace(const char *elf_file)
 
     // 检查是不是 ELF 文件（魔数检查 \x7fELF）
     // e_ident 是一个数组，是 ELF 文件的魔数 和基本信息，用来确认文件是 ELF 格式，并描述 ELF 文件的一些基本属性。
-    if (strncmp((char *)ehdr.e_ident, "\x7f" "ELF",4) != 0)
+    if (strncmp((char *)ehdr.e_ident, "\x7f"
+                                      "ELF",
+                4) != 0)
     {
         printf("Error: Not an ELF file\n");
         fclose(fp);
@@ -65,7 +66,7 @@ void init_ftrace(const char *elf_file)
         exit(1);
     }
 
-    //读取节区字符串表（shstrtab），用于找到.symtab和.strtab
+    // 读取节区字符串表（shstrtab），用于找到.symtab和.strtab
     char *shstrtab = malloc(shdr[ehdr.e_shstrndx].sh_size); // ehdr.e_shstrndx 得到 .shstrtab 对应的节区头，然后通过它的 sh_offset 等信息来读取节区名称字符串表的内容。
     if (!shstrtab)
     {
@@ -86,15 +87,16 @@ void init_ftrace(const char *elf_file)
 
     // 查找.symtab和.strtab所在的节区
     Elf32_Shdr *symtab_shdr = NULL, *strtab_shdr = NULL; // symtab_shdr，指向符号表节区头的指针，保存符号表的位置和大小，strtab_shdr：指向字符串表节区头的指针，保存字符串表的位置和大小。
-    for (int i = 0; i < ehdr.e_shnum;i++)
+    for (int i = 0; i < ehdr.e_shnum; i++)
     {
         char *name = shstrtab + shdr[i].sh_name;
-        if(strcmp(name,".symtab") == 0) symtab_shdr = &shdr[i];
-        if(strcmp(name,".strtab") == 0) strtab_shdr = &shdr[i];
+        if (strcmp(name, ".symtab") == 0)
+            symtab_shdr = &shdr[i];
+        if (strcmp(name, ".strtab") == 0)
+            strtab_shdr = &shdr[i];
     }
 
-
-    if(!symtab_shdr || !strtab_shdr)
+    if (!symtab_shdr || !strtab_shdr)
     {
         printf("ErrorL.symtab or .strtab not Found\n");
         free(shstrtab);
@@ -127,7 +129,7 @@ void init_ftrace(const char *elf_file)
 
     // 读取符号表
     int sym_count = symtab_shdr->sh_size / sizeof(Elf32_Sym); // 计算符号表中总共有多少个符号（symbol）项，符号数量 = 符号表总大小 / 单个符号大小
-    Elf32_Sym *symtab = malloc(symtab_shdr->sh_size); // 符号表数据的指针数组，包含每个符号的详细信息
+    Elf32_Sym *symtab = malloc(symtab_shdr->sh_size);         // 符号表数据的指针数组，包含每个符号的详细信息
     if (!symtab)
     {
         printf("Error: Failed to allocate memory for symtab\n");
@@ -150,10 +152,10 @@ void init_ftrace(const char *elf_file)
     }
 
     func_count = 0;
-    for (int i = 0; i < sym_count;i++)
+    for (int i = 0; i < sym_count; i++)
     {
         // ELF32_ST_TYPE(x)：是一个宏，用来提取 st_info 里表示符号类型的低 4 位，STT_FUNC：是一个枚举常量，表示 "函数" 类型的符号（function symbol），数值通常是 2
-        if(ELF32_ST_TYPE(symtab[i].st_info) == STT_FUNC)
+        if (ELF32_ST_TYPE(symtab[i].st_info) == STT_FUNC)
         {
             func_count++;
         }
@@ -170,10 +172,10 @@ void init_ftrace(const char *elf_file)
         fclose(fp);
         exit(1);
     }
-    int idx = 0;//下标
-    for (int i = 0; i < sym_count;i++)
+    int idx = 0; // 下标
+    for (int i = 0; i < sym_count; i++)
     {
-        if(ELF32_ST_TYPE(symtab[i].st_info) == STT_FUNC)
+        if (ELF32_ST_TYPE(symtab[i].st_info) == STT_FUNC)
         {
             func_symbols[idx].name_offset = symtab[i].st_name;
             func_symbols[idx].value = symtab[i].st_value;
@@ -190,13 +192,12 @@ void init_ftrace(const char *elf_file)
     Log("ftrace intialized with %d function symbols from %s", func_count, elf_file);
 }
 
-
 const char *addr_to_func(uint32_t addr)
 {
-    for (int i = 0; i < func_count;i++)
+    for (int i = 0; i < func_count; i++)
     {
         // 判断当前地址 addr，是不是落在这个函数的地址范围内（[value, value+size)，value：函数在运行时的虚拟地址
-        if (addr >= func_symbols[i].value && addr < func_symbols[i].value + func_symbols[i].size) 
+        if (addr >= func_symbols[i].value && addr < func_symbols[i].value + func_symbols[i].size)
         {
             return strtab_data + func_symbols[i].name_offset;
         }
@@ -205,13 +206,12 @@ const char *addr_to_func(uint32_t addr)
     return "???";
 }
 
-
 uint32_t func_to_addr(const char *func_name)
 {
-    for (int i = 0; i < func_count;i++)
+    for (int i = 0; i < func_count; i++)
     {
         const char *name = strtab_data + func_symbols[i].name_offset;
-        if (strcmp(name,func_name)==0)
+        if (strcmp(name, func_name) == 0)
         {
             return func_symbols[i].value;
         }
@@ -236,7 +236,7 @@ void ftrace_call(uint32_t pc, uint32_t target_addr)
 void ftrace_ret(uint32_t pc)
 {
     const char *func_name = addr_to_func(pc);
-    
+
     if (indent_level > 0)
         indent_level--;
 
@@ -247,6 +247,5 @@ void ftrace_ret(uint32_t pc)
         printf("  ");
     }
 
-    printf(" %-6s [%s]\n",  "ret", func_name);
+    printf(" %-6s [%s]\n", "ret", func_name);
 }
-
