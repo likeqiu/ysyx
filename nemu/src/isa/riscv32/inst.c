ff0af306ffdf47a92356cbc5f7265d3bcb6d35f7
tracer-ysyx <tracer@ysyx.org>
2025-04-29 10:01:16 -0400

    > compile NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:01:16 up 18:49,  1 user,  load average: 0.01, 0.01, 0.00

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -133,8 +133,26 @@ static int decode_exec(Decode *s) {
   //       B型指令格式
   //        [31] [30:25] [24:20] [19:15] [14:12] [11:8] [7]  [6:0]
   //       imm[12] imm[10:5]  rs2     rs1     funct3  imm[4:1] imm[11] opcode
-  INSTPAT("??????? 00000 ????? 001 ????? 11000 11", bnez, B, if (src1 != 0) s->dnpc = s->pc + imm);
- // INSTPAT("??????? ????? 00000 101 ????? 11000 11", blez, B, if ((int32_t)src1 <= 0) s->dnpc = s->pc + imm); // 允许负数比较 ,这个指令很容易出问题
+  INSTPAT("??????? 00000 ????? 001 ????? 11000 11", bnez, B, {
+    bool taken = ((src1 != 0));
+    if (taken)
+      s->dnpc = s->pc + imm;
+
+    for (int i = 0; i < branch_count; i++)
+    {
+      if (branch_stats[i].pc == s->pc)
+      {
+        branch_stats[i].count++;
+        branch_stats[i].taken = taken;
+        return 0;
+      }
+    }
+
+    branch_stats = realloc(branch_stats, (branch_count + 1) * sizeof(BranchStat));
+    branch_stats[branch_count] = (BranchStat){s->pc, s->pc + imm, 1, taken};
+    branch_count++;
+  });
+  // INSTPAT("??????? ????? 00000 101 ????? 11000 11", blez, B, if ((int32_t)src1 <= 0) s->dnpc = s->pc + imm); // 允许负数比较 ,这个指令很容易出问题
   // src2 作为操作数，判断是否满足跳转条件。src1是用来和其他寄存器来比较，比如 BEQ、BNE
   INSTPAT("??????? ????? 00001 101 ????? 11000 11", bgez, B, {
     bool taken = ((int32_t)src1 >= 0);
