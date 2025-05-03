9291d20e2862d00def2b34885195e259ac02c69c
tracer-ysyx <tracer@ysyx.org>
2025-05-03 09:24:07 -0400

    > compile NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:24:07 up 2 days, 18:18,  1 user,  load average: 0.01, 0.00, 0.00

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -26,7 +26,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
   for (int i = 0; i < 32; i++)
   {
 
-    cpu.gpr[0] = 1;
+
     if (i == 0 && (cpu.gpr[0] != 0 || ref_r->gpr[0] != 0))
     {
       Log("Register %s mismatch: DUT = 0x%08x, REF = 0x%08x at PC = 0x%08x\n",
