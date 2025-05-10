4d18914d78203d2b75600626630ada2fb69070ac
tracer-ysyx <tracer@ysyx.org>
2025-05-10 09:29:46 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:29:46 up 12:14,  1 user,  load average: 0.02, 0.05, 0.05

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <nemu.h>
-
+#include<stdio.h>
 // 注释：NEMU 的 RTC 寄存器由模拟器自动维护，无需初始化
 void __am_timer_init() {
 }
@@ -11,6 +11,7 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t low = inl(RTC_ADDR);
   // 低地址存储的是低 32 位，高地址存储的是高 32 位，这是小端内存布局
 
+  printf("RTC_ADDR = 0x%lx, high = 0x%x, low = 0x%x\n", RTC_ADDR, high, low);
   uptime->us = ((uint64_t)high << 32) | low;
 }
 
