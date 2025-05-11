6a35a1db9c0220a34f5d15a676943a03ba5c637d
tracer-ysyx <tracer@ysyx.org>
2025-05-11 10:47:18 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:47:18 up 18:53,  1 user,  load average: 0.01, 0.96, 1.13

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -124,7 +124,9 @@ static void execute(uint64_t n) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
-    if (nemu_state.state != NEMU_RUNNING) break;
+    printf("1111\n");
+    if (nemu_state.state != NEMU_RUNNING)
+      break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
 }
