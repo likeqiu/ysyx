bf259d85ce3287d6c91fad4a5bc15a5c026ca8af
tracer-ysyx <tracer@ysyx.org>
2025-05-11 21:38:01 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:38:01 up 19:29,  1 user,  load average: 0.06, 0.07, 0.17

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -41,10 +41,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   //这里注释掉日志就不会出现两行相同指令
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
-
-  
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-
 
 #ifdef CONFIG_WATCHPOINT
   WP *wp=head ;
