2b4c65377c0bf5527592bbf93caaa6642c8fe7bf
tracer-ysyx <tracer@ysyx.org>
2025-05-13 06:12:49 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  06:12:49 up 1 day,  6:58,  1 user,  load average: 0.02, 0.03, 0.10

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -163,6 +163,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000001 ????? ????? 000 ????? 01100 11", mul, R, R(rd) = ((int32_t)src1 * (int32_t)src2) & 0xffffffff); // 取低32位
   INSTPAT("0000001 ????? ????? 001 ????? 01100 11", mulh, R, R(rd) = ((int64_t)(int32_t)src1 * (int64_t)(int32_t)src2) >> 32);
   INSTPAT("0000001 ????? ????? 100 ????? 01100 11", div, R, R(rd) = (src2 == 0) ? -1 : ((src1 == INT32_MIN && src2 == -1) ? INT32_MIN : (int32_t)src1 / (int32_t)src2)); // 除0等于-1
+  INSTPAT("0000001 ????? ????? 101 ????? 01100 11", divu, R, R(rd) = (src2 == 0) ? 0xFFFFFFFF : (uint32_t)src1 / (uint32_t)src2);
   INSTPAT("0000001 ????? ????? 110 ????? 01100 11", rem, R, R(rd) = (src2 == 0) ? src1 : ((src1 == INT32_MIN && src2 == -1) ? 0 : (int32_t)src1 % (int32_t)src2));
   INSTPAT("0000001 ????? ????? 111 ????? 01100 11", remu, R, R(rd) = (src2 == 0) ? src1 : (uint32_t)src1 % (uint32_t)src2);
   // 当src2为0，余数为src1, (src1 == INT32_MIN && src2 == -1)防止溢出，其他情况不会溢出
@@ -174,7 +175,7 @@ static int decode_exec(Decode *s) {
 
     
     
-    INSTPAT("0000001 ????? ????? 101 ????? 01100 11", divu, R, R(rd) = (src2 == 0) ? 0xFFFFFFFF : (uint32_t)src1 / (uint32_t)src2);
+    
     INSTPAT("0000001 ????? ????? 011 ????? 01100 11", mulhu, R, R(rd) = ((uint64_t)(uint32_t)src1 * (uint64_t)(uint32_t)src2) >> 32);
     INSTPAT("0000001 ????? ????? 010 ????? 01100 11", mulhsu, R, R(rd) = ((int64_t)(int32_t)src1 * (uint64_t)(uint32_t)src2) >> 32);*/
   //INSTPAT("0000111 11110 ????? 111 ????? 00100 11", zext_b, I, R(rd) = src1 & 0xFF);
