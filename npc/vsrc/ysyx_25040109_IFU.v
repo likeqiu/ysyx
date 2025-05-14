3fe7fabf922ea69813f5351f5e9a3bd6e762f35f
tracer-ysyx <tracer@ysyx.org>
2025-05-14 10:10:45 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:10:45 up 1 day, 19:13,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/vsrc/ysyx_25040109_IFU.v
+++ npc/vsrc/ysyx_25040109_IFU.v
@@ -1,12 +1,12 @@
 module ysyx_25040109_IFU (
+/* verilator lint_off UNUSEDSIGNAL */
     input clk,
     input [31:0] pc,
+    /* verilator lint_on UNUSEDSIGNAL */
     input [31:0] inst,
     output [31:0] inst_ifu
 );
-    /* verilator lint_off UNUSEDSIGNAL */
-    // clk 和 pc 当前未使用，保留以便未来扩展
+
     assign inst_ifu = inst;
-    /* verilator lint_on UNUSEDSIGNAL */
 endmodule
 
