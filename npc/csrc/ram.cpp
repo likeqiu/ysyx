6c80da0cf87fabfc079581732a5421cc11b17575
tracer-ysyx <tracer@ysyx.org>
2025-05-15 03:09:04 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:09:04 up 1 day, 20:38,  1 user,  load average: 0.06, 0.02, 0.00

--- npc/csrc/ram.cpp
+++ npc/csrc/ram.cpp
@@ -89,13 +89,21 @@ int main(int argc,char **argv){
     int cycle = 0;
     vluint64_t sim_time = 0;
     // vl 前缀表示 Verilator Long（Verilator专用）
-
     top->rst = 1;
+    for (int i = 0; i < 2; i++)
+    { // 两个时钟周期
+        top->clk = 0;
+        top->eval();
+        tfp->dump(sim_time++);
+        top->clk = 1;
+        top->eval();
+        tfp->dump(sim_time++);
+    }
+    top->rst = 0;
     top->clk = 0;
-
     top->eval();
     tfp->dump(sim_time++);
-    top->rst = 0;
+    cout << "After reset: PC = 0x" << hex << top->pc << dec << endl;
 
     /*Verilated::gotFinish() 是 Verilator 仿真库中的一个 静态函数，用于判断 Verilog 仿真模型是否调用了 $finish 系统任务。*/
     while(cycle < max_cycles &&  !Verilated::gotFinish())
