441520291535d24d159d9b992c81fc0676d551ba
tracer-ysyx <tracer@ysyx.org>
2025-05-16 08:58:11 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:58:11 up 2 days,  1:16,  1 user,  load average: 0.02, 0.04, 0.04

--- npc/vsrc/ysyx_25040109_Reg.v
+++ npc/vsrc/ysyx_25040109_Reg.v
@@ -1,12 +1,16 @@
-module ysyx_25040109_Reg #(WIDTH = 1, RESET_VAL = 0) (
+module ysyx_25040109_Reg #(WIDTH = 1, RESET_VAL = 0, RESET_INST=0) (
   input clk,
   input rst,
   input [WIDTH-1:0] din,
   output reg [WIDTH-1:0] dout,
-  input wen
+  input wen,
+  output [31:0] inst_ifu
 );
   always @(posedge clk) begin
-    if (rst) dout <= RESET_VAL;
+    if (rst) begin 
+    dout <= RESET_VAL;
+    inst_ifu<=RESET_INST;
+    end
     else if (wen) dout <= din;
   end
 endmodule
