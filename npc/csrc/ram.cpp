5ff853bc872373d0e7aa557835d6aeb6793af559
tracer-ysyx <tracer@ysyx.org>
2025-05-16 08:34:17 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:34:17 up 2 days, 52 min,  1 user,  load average: 0.22, 0.07, 0.02

--- npc/csrc/ram.cpp
+++ npc/csrc/ram.cpp
@@ -108,10 +108,11 @@ int main(int argc,char **argv){
     vluint64_t sim_time = 0;
     // vl 前缀表示 Verilator Long（Verilator专用）
 
+    top->rst = 1;
     top->clk = 0;
     top->eval();
     tfp->dump(sim_time++);
-
+    top->rst = 0;
     top->inst = imem.pmem_read(top->pc);
 
     top->clk = 1;
