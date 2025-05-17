7c97a382885518f32157b10677f22e1a334b8856
tracer-ysyx <tracer@ysyx.org>
2025-05-16 20:59:16 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:59:16 up 2 days,  4:56,  1 user,  load average: 0.43, 0.12, 0.04

--- npc/csrc/ram.cpp
+++ npc/csrc/ram.cpp
@@ -70,22 +70,7 @@ public:
     }
 };
 
-void single_cycle()
-{
-    top->clk = 0;
-    top->eval();
 
-    top->clk = 1;
-    top->eval();
-}
-
-void reset(int n)
-{
-    top->rst = 1;
-    while (n-- > 0)
-        single_cycle();
-    top->rst = 0;
-}
 
 int main(int argc,char **argv){
 
