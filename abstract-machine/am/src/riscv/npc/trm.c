851b8107e094aabf9a6705545a28db0cc5935744
tracer-ysyx <tracer@ysyx.org>
2025-05-18 00:58:59 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:58:59 up 2 days, 22:57,  1 user,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -16,6 +16,10 @@ void putch(char ch) {
 
 void halt(int code) {
   asm volatile("mv a0, %0; ebreak" : : "r"(code));
+  // 相当于npc_trap(code);
+  //  asm: 关键字，表示后面的内容是汇编指令。
+  /*volatile : 这是内联汇编的一个修饰符。它告诉编译器 不要 对这段汇编代码进行优化（例如，移除它、重排它）或者认为它没有副作用。这对于像 ebreak 这种会改变程序流程或与外部环境交互的指令至关重要。
+  总结来说，nemu_trap(code) 宏是在被模拟程序中，将一个表示特定状态或信息的数值 code 放入 a0 寄存器，然后执行 ebreak 指令，从而通知 NEMU 模拟器发生了某个特定事件（其类型由 code 的值指示）。NEMU 在捕获到 ebreak 陷阱后，会检查 a0 寄存器的值来确定如何处理（例如，根据 code 的值判断程序是正常退出还是出错）。*/
   while (1);
 }
 
