300fa301cb7c525f2a81bb5e66dc79b9e3d6a6cf
tracer-ysyx <tracer@ysyx.org>
2025-05-21 10:26:31 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:26:31 up 3 days, 13:14,  1 user,  load average: 0.08, 0.02, 0.01

--- npc/csrc/sdb.cpp
+++ npc/csrc/sdb.cpp
@@ -3,6 +3,10 @@
 #include <cstdlib>
 using namespace std;
 
+
+
+
+
 static char* rl_gets(){
     static char *line_read = nullptr;
 
@@ -10,7 +14,7 @@ static char* rl_gets(){
         free(line_read);
         line_read = nullptr;
     }
-    // readline 会动态分配一块内存，用于存储用户输入的字符串
+    // readline 会动态分配一块内存，用于存储用户输入的字符串(整行)（如果前别有空格也写入）
     line_read = readline("(npc) ");
 
     // 确保 line_read 是一个有效的指针（非 NULL），并且用户输入的字符串不为空（至少有一个字符）。
@@ -31,11 +35,64 @@ static struct {
 
 };
 
+#define NR_CMD (int)(sizeof(cmd_table) / sizeof(cmd_table[0]))
+
 static int cmd_help(char *args){
-    char *arg = strtok(NULL, " ");
+    char *arg = strtok(nullptr, " ");
     int i;
 
-    if (arg==NULL){
-        for(u=0)
+    if (arg == nullptr)
+    {
+        for ( i = 0; i < NR_CMD;i++)
+        {
+            printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        }
+    }
+    else
+    {
+        for (i = 0; i < NR_CMD;i++)
+        {
+            if(strcmp(arg,cmd_table[i].name)==0)
+            {
+                printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+                return 0;
+            }
+        }
+
+        printf("Unkonw command '%s'\n", arg);
+    }
+
+    return 0;
+}
+
+void sdb_mainloop()
+{
+    for (char *str;(str=rl_gets())!=nullptr;)
+    {
+        char *str_end = str + strlen(str);
+        char *cmd = strtok(str, " ");
+
+        if(cmd==NULL){continue;}
+
+        char *args = cmd + strlen(cmd) + 1;
+        if(args >= str_end){
+            args = NULL;
+        }
+    }
+
+    int i;
+    for (i = 0; i < NR_CMD;i++)
+    {
+        if (strcmp(cmd, cmd_table[i].name) == 0)
+        {
+            if(cmd_table[i].handler(args) < 0){
+                return;
+            }
+            break;
+        }
+    }
+
+    if(i==NR_CMD){
+        printf("Unknown comand '%s'\n", cmd);
     }
 }
