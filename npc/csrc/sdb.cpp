c0b386c2fb3dd6fa1a4bf839fb5978897510576c
tracer-ysyx <tracer@ysyx.org>
2025-05-22 07:34:34 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:34:34 up 3 days, 15:26,  1 user,  load average: 0.26, 0.11, 0.04

--- npc/csrc/sdb.cpp
+++ npc/csrc/sdb.cpp
@@ -7,8 +7,7 @@ using namespace std;
 
 extern Vysyx_25040109_top *top;
 
-static char *
-rl_gets()
+static char *rl_gets()
 {
     static char *line_read = nullptr;
 
@@ -32,7 +31,7 @@ static int cmd_help(char *args);
 extern "C" void print_registers();
 static int cmd_info(char *args)
 {
-    const char *scope_path = "ysyx_25040109_top"; // <-- 修改这里，加上 'V' 前缀
+    const char *scope_path = "Vysyx_25040109_top"; // <-- 修改这里，加上 'V' 前缀
 
     svScope current_scope = svGetScopeFromName(scope_path);
 
