7d8845e1bbc3acf3fba0f535b0f9c26a45081e0b
tracer-ysyx <tracer@ysyx.org>
2025-05-22 09:33:53 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:33:53 up 3 days, 17:21,  1 user,  load average: 0.05, 0.05, 0.03

--- npc/csrc/sdb.cpp
+++ npc/csrc/sdb.cpp
@@ -41,7 +41,31 @@ static int cmd_info(char *args)
 
 static int cmd_c(char *args)
 {
-   
+
+    while (!Verilated::gotFinish())
+    {
+
+        top->clk = 0;
+        top->eval();
+        tfp->dump(sim_time++);
+
+        /*try-catch 这就是一个会“抛出异常”的函数。如果你没有用 try-catch，程序会崩溃终止。有了 try-catch，就可以优雅地退出仿真，同时打印有用的调试信息。*/
+        try
+        {
+            top->inst = imem.pmem_read(top->pc);
+        }
+        catch (const exception &e)
+        {
+            cerr << "Cycle" << ":" << e.what() << endl;
+            /*e.what() 是 C++ 中异常类（如 std::exception）的一个成员函数，用于返回异常的详细信息（错误描述），类型为 const char,抛出的信息取决于前边定义的throw内容*/
+            break;
+        }
+
+        top->clk = 1;
+        top->eval();
+        tfp->dump(sim_time++);
+    }
+
     return 0;
 }
 
