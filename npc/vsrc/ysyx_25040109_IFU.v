8dd12dffc2a3ef71e835f0571065727a8c241506
tracer-ysyx <tracer@ysyx.org>
2025-05-25 05:45:24 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:45:24 up 4 days, 18:28,  1 user,  load average: 0.21, 0.09, 0.03

--- npc/vsrc/ysyx_25040109_IFU.v
+++ npc/vsrc/ysyx_25040109_IFU.v
@@ -9,12 +9,18 @@ module ysyx_25040109_IFU (
     import "DPI-C" function void pmem_read(input int addr,output int data);
         
 
+ 
     wire [31:0] inst_raw;
-    always @(posedge clk) begin
-        pmem_read(pc,inst_raw);
-    end
-    
-    assign inst_ifu=inst_raw;
+   assign inst_raw = pmem_read(pc, inst_raw);
+
+    // 使用 Reg 模板存储指令
+    ysyx_25040109_Reg #(32, 32'h0) inst_reg (
+        .clk(clk),
+        .rst(1'b0),
+        .din(inst_raw),
+        .dout(inst_ifu),
+        .wen(1'b1) // 每周期更新指令
+    );
     /* inst_raw 是 临时变量，类型是 wire，可以通过 DPI-C 调用赋值。
     inst_ifu 是模块的 特定输出端口，默认是 wire，不能在 always 块中直接赋值，除非声明为 reg。DPI-C 不知道怎么处理它的“方向性”，所以不允许直接赋值。*/
 endmodule
