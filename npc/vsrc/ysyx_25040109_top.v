596c1310c113d68433f4616c209dfb8cd523f125
tracer-ysyx <tracer@ysyx.org>
2025-05-24 21:59:56 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:59:56 up 4 days, 11:47,  1 user,  load average: 0.01, 0.03, 0.00

--- npc/vsrc/ysyx_25040109_top.v
+++ npc/vsrc/ysyx_25040109_top.v
@@ -81,12 +81,10 @@ module ysyx_25040109_top (
             $display("PC=0x%h, inst=0x%h,",pc, inst_ifu);
         end
 
-         if(monitor_pc(pc) != 0)begin
+         if(monitor_pc(pc) != 0  || printf_finish(inst) == 0)begin
             $finish;
          end
-        if(printf_finish(inst) == 0 )begin
-            $finish;
-        end
+       
 
     end
 
