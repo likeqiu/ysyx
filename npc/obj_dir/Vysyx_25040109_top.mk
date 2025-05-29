dd37ab616a15215b876915c4b4ddbc0b1c5e2322
tracer-ysyx <tracer@ysyx.org>
2025-05-29 03:47:06 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:47:06 up 12:42,  1 user,  load average: 0.19, 0.10, 0.03

--- npc/obj_dir/Vysyx_25040109_top.mk
+++ npc/obj_dir/Vysyx_25040109_top.mk
@@ -59,6 +59,7 @@ VM_USER_LDLIBS = \
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
 	expr \
+	ftrace \
 	mtrace \
 	ram \
 	reg \
@@ -80,6 +81,8 @@ include $(VERILATOR_ROOT)/include/verilated.mk
 VPATH += $(VM_USER_DIR)
 
 expr.o: csrc/expr.c
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+ftrace.o: csrc/ftrace.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 mtrace.o: csrc/mtrace.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
