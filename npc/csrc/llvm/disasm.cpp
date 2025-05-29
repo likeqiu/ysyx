f7506234ffe0f55a4c09546d8b5ef966d0fea75a
tracer-ysyx <tracer@ysyx.org>
2025-05-28 20:31:59 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:31:59 up  8:55,  1 user,  load average: 0.01, 0.02, 0.02

--- npc/csrc/llvm/disasm.cpp
+++ npc/csrc/llvm/disasm.cpp
@@ -108,7 +108,7 @@ static void objdump_disasm(long long pc, unsigned int instruction_word, int inst
     {
         bytes[i] = (instruction_word >> (i * 8)) & 0xFF;
     }
-    ssize_t temp=write(fd, bytes, instr_len_bytes);
+    (void)write(fd, bytes, instr_len_bytes);
     close(fd);
 
     // 使用objdump反汇编
