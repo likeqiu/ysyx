db0b4f6c304b37bed7da4a61b29f1a021ea5f253
tracer-ysyx <tracer@ysyx.org>
2025-05-31 06:18:55 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  06:18:55 up 1 day,  6:51,  1 user,  load average: 0.19, 0.16, 0.07

--- npc/csrc/paddr.cpp
+++ npc/csrc/paddr.cpp
@@ -10,7 +10,7 @@ uint8_t *guest_to_host(paddr_t paddr) {
 paddr_t gost_to_guest(uint8_t *haddr) {
     // ptrdiff_t 是一种有符号整型，用于表示两个指针相减的结果。
     ptrdiff_t offset = haddr - pmem.data();
-    if(offset < 0 || offset >= static_case<ptrdiff_t>(pmem.get_size())){
+    if(offset < 0 || offset >= static_cast<ptrdiff_t>(pmem.get_size())){
         throw std::out_of_range("Host address out of range");
     }
 
