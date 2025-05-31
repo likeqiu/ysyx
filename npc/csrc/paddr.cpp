47b5144abdeea37a0251a1606f0f097eee2c5819
tracer-ysyx <tracer@ysyx.org>
2025-05-31 06:25:03 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  06:25:03 up 1 day,  6:57,  1 user,  load average: 0.05, 0.06, 0.05

--- npc/csrc/paddr.cpp
+++ npc/csrc/paddr.cpp
@@ -1,18 +1,21 @@
 #include "ram.h"
+#include <stdexcept>
 
-uint8_t *guest_to_host(paddr_t paddr) {
-    if (!pmem.in_pmem(paddr) {
-            throw std::out_of_range("Address out of range:" + std::to_string(paddr));
-        })
-
-        return pmem.data() + (paddr - CONFIG_MBASE);
+uint8_t *guest_to_host(paddr_t paddr)
+{
+    if (paddr < PMEM_LEFT || paddr > PMEM_RIGHT)
+    {
+        throw std::out_of_range("Address out of range: " + std::to_string(paddr));
+    }
+    return pmem.pmem.data() + (paddr - CONFIG_MBASE);
 }
-paddr_t gost_to_guest(uint8_t *haddr) {
-    // ptrdiff_t 是一种有符号整型，用于表示两个指针相减的结果。
-    ptrdiff_t offset = haddr - pmem.data();
-    if(offset < 0 || offset >= static_cast<ptrdiff_t>(pmem.get_size())){
+
+paddr_t gost_to_guest(uint8_t *haddr)
+{
+    ptrdiff_t offset = haddr - pmem.pmem.data();
+    if (offset < 0 || offset >= pmem.get_size())
+    {
         throw std::out_of_range("Host address out of range");
     }
-
     return static_cast<paddr_t>(offset + CONFIG_MBASE);
 }
