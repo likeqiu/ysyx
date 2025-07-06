3c8c25417dccdbd30cd3165042a48c86522d4044
tracer-ysyx <tracer@ysyx.org>
2025-07-06 07:50:43 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:50:43 up 1 day, 23:30,  1 user,  load average: 0.83, 0.98, 0.86

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -21,7 +21,7 @@ extern uint64_t g_nr_guest_inst;
 FILE *log_fp = NULL;
 
 void init_log(const char *log_file) {
-  log_fp = stdout;
+  log_fp = stdout; // 将日志输出设置为标准输出,输出到终端
   if (log_file != NULL) {
     FILE *fp = fopen(log_file, "w");
     Assert(fp, "Can not open '%s'", log_file);
--- npc/include/common.h
+++ npc/include/common.h
@@ -7,15 +7,32 @@
 #include <iostream>  //提供了输入/输出流的功能。
 #include <fstream> //提供了文件流的功能。
 #include <cstdio>
-#include<cassert>
-#include<cstring>
+#include <cassert>
+#include <cstring>
 
 #include <generated/autoconf.h>
 #include <macro.h>
 
-#define word_t uint32_t
-typedef word_t vaddr_t;
-typedef uint32_t paddr_t;
+#ifdef CONFIG_TARGET_AM
+#include <klib.h>
+#else
+#include <assert.h>
+#include <stdlib.h>
+#endif
 
+#if CONFIG_MBASE + CONFIG_MSIZE > 0x100000000ul
+#define PMEM64 1
+#endif
+
+typedef MUXDEF(CONFIG_ISA64, uint64_t, uint32_t) word_t;
+typedef MUXDEF(CONFIG_ISA64, int64_t, int32_t) sword_t;
+#define FMT_WORD MUXDEF(CONFIG_ISA64, "0x%016" PRIx64, "0x%08" PRIx32)
+
+typedef word_t vaddr_t;
+typedef MUXDEF(PMEM64, uint64_t, uint32_t) paddr_t;
+#define FMT_PADDR MUXDEF(PMEM64, "0x%016" PRIx64, "0x%08" PRIx32)
+typedef uint16_t ioaddr_t;
+
+#include <debug.h>
 
 #endif
