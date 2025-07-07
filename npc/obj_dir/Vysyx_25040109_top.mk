5c3107a89762197469ea4f32c6e3c870a4d6fa3a
tracer-ysyx <tracer@ysyx.org>
2025-07-07 08:49:05 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:49:05 up 2 days, 12:11,  1 user,  load average: 0.70, 0.63, 0.63

--- npc/obj_dir/Vysyx_25040109_top.mk
+++ npc/obj_dir/Vysyx_25040109_top.mk
@@ -47,15 +47,7 @@ VM_USER_CFLAGS = \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
-	/home/zxj17/ysyx-workbench/npc/csrc/llvm/disasm.o \
 	-lz \
-	-lreadline \
-	-lhistory \
-	-lncurses \
-	-lSDL2 \
-	-lSDL2_image \
-	-lSDL2_ttf \
-	-ldl \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
