37a1a54c4294992fdb52da09dee36970292fccc0
tracer-ysyx <tracer@ysyx.org>
2025-07-07 08:56:45 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:56:45 up 2 days, 12:18,  1 user,  load average: 0.35, 0.65, 0.65

--- npc/include/debug.h
+++ npc/include/debug.h
@@ -18,10 +18,11 @@
                    (fflush(stdout), fprintf(stderr, ANSI_FMT(format, ANSI_FG_RED) "\n", ##__VA_ARGS__))); \
             IFNDEF(CONFIG_TARGET_AM, extern FILE * log_fp; fflush(log_fp));                               \
             extern void assert_fail_msg();                                                                \
-            assert_fail_msg();                                                                            \
             assert(cond);                                                                                 \
         }                                                                                                 \
     } while (0)
+
+// assert_fail_msg()添加到上边定义的
 
 #define panic(format, ...) Assert(0, format, ##__VA_ARGS__)
 
