04cb307f339b395295370dc738e4265434fab79c
tracer-ysyx <tracer@ysyx.org>
2025-07-12 22:54:30 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:54:30 up 4 days,  7:00,  1 user,  load average: 0.97, 0.75, 0.71

--- npc/vsrc/ysyx_25040109_IFU.v
+++ npc/vsrc/ysyx_25040109_IFU.v
@@ -29,7 +29,7 @@ module ysyx_25040109_IFU (
         if (!rst && inst_valid) begin
             verilog_pmem_read(pc_reg, temp_inst_ifu);
         end else begin
-            temp_inst_ifu =32'b0;
+            temp_inst_ifu <=32'b0;
         end
         inst_ifu <= temp_inst_ifu;
 
