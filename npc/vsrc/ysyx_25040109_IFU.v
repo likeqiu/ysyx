f4109ca1f8c4482bb6bd2668fe352a507e12fd89
tracer-ysyx <tracer@ysyx.org>
2025-07-13 02:41:19 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:41:19 up 4 days,  9:22,  1 user,  load average: 0.94, 0.88, 0.88

--- npc/vsrc/ysyx_25040109_IFU.v
+++ npc/vsrc/ysyx_25040109_IFU.v
@@ -1,20 +1,17 @@
 module ysyx_25040109_IFU (
     input clk,
     input rst,
-    input [31:0] pc,           
-    output [31:0] inst_ifu,
+    input [31:0] pc,          // 当前PC
+    output [31:0] inst_ifu,   // 当前PC对应的指令
     output inst_valid
 );
-    reg [31:0] inst_reg;
-    reg inst_valid_reg;
-    
     wire is_pc_valid = (pc >= 32'h80000000) && (pc <= 32'h87FFFFFF) && (pc[1:0] == 2'b00);
     
     import "DPI-C" function void verilog_pmem_read(input int addr, output int data);
     
     reg [31:0] inst_from_mem;
     
-    // 组合逻辑读取指令
+    // 组合逻辑直接读取当前PC的指令
     always @(*) begin
         if (is_pc_valid) begin
             verilog_pmem_read(pc, inst_from_mem);
@@ -23,19 +20,9 @@ module ysyx_25040109_IFU (
         end
     end
     
-    // 时序逻辑更新输出
-    always @(posedge clk) begin
-        if (rst) begin
-            inst_reg <= 32'h00000013;
-            inst_valid_reg <= 1'b0;
-        end else begin
-            inst_reg <= inst_from_mem;
-            inst_valid_reg <= is_pc_valid;
-        end
-    end
-    
-    assign inst_ifu = inst_reg;
-    assign inst_valid = inst_valid_reg;
+    // 直接输出，不延迟
+    assign inst_ifu = inst_from_mem;
+    assign inst_valid = is_pc_valid;
 endmodule
 
 
