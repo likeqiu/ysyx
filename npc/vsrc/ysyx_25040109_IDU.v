a9fbfc8e4e053035eb5fa744fcdfa66ad656537d
tracer-ysyx <tracer@ysyx.org>
2025-07-14 05:26:27 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:26:27 up 4 days, 10:41,  1 user,  load average: 0.77, 0.81, 0.84

--- npc/vsrc/ysyx_25040109_IDU.v
+++ npc/vsrc/ysyx_25040109_IDU.v
@@ -14,15 +14,15 @@ module ysyx_25040109_IDU (
 
     wire [11:0] imm_i = inst[31:20];
     wire [19:0] imm_u = inst[31:12];
-    wire [20:1] imm_j = {inst[31], inst[19:12], inst[20], inst[30:21]};
+    wire [20:0] imm_j = {inst[31], inst[19:12], inst[20], inst[30:21], 1'b0};
     wire [11:0] imm_s = {inst[31:25], inst[11:7]};
-    wire [12:1] imm_b = {inst[31], inst[7], inst[30:25], inst[11:8]};
+    wire [12:0] imm_b = {inst[31], inst[7], inst[30:25], inst[11:8], 1'b0};
 
     wire [31:0] imm_i_ext = {{20{imm_i[11]}}, imm_i};
     wire [31:0] imm_u_ext = {imm_u, 12'b0};
-    wire [31:0] imm_j_ext = {{11{imm_j[20]}}, imm_j, 1'b0};
+    wire [31:0] imm_j_ext = {{12{imm_j[20]}}, imm_j[20:1]};
     wire [31:0] imm_s_ext = {{20{imm_s[11]}}, imm_s};
-    wire [31:0] imm_b_ext = {{19{imm_b[12]}}, imm_b, 1'b0};
+    wire [31:0] imm_b_ext = {{20{imm_b[12]}}, imm_b[12:1]};   
 
     ysyx_25040109_MuxKeyWithDefault #(7, 7, 32) imm_select
     (
