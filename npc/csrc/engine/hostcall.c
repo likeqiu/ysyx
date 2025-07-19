90d65652f5551a0657399d38d4d9895055cf585b
tracer-ysyx <tracer@ysyx.org>
2025-07-19 05:25:11 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:25:11 up  5:27,  5 users,  load average: 0.08, 0.03, 0.01

--- npc/csrc/engine/hostcall.c
+++ npc/csrc/engine/hostcall.c
@@ -4,7 +4,7 @@
 #include<cpu/difftest.h>
 
 void set_nemu_state(int state, vaddr_t pc,int halt_ret){
-    difftest_skip_ref();
+    //difftest_skip_ref();
     npc_state.state = state;
     npc_state.halt_pc = pc;
     npc_state.halt_ret = halt_ret;
