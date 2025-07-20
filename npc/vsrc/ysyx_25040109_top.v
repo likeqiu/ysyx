1f6c02345b9c3dbe6d1e2dc8d0867b3e3eb09ecb
tracer-ysyx <tracer@ysyx.org>
2025-07-19 23:21:55 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:21:55 up 10:25,  5 users,  load average: 0.51, 0.43, 0.26

--- npc/vsrc/ysyx_25040109_top.v
+++ npc/vsrc/ysyx_25040109_top.v
@@ -136,7 +136,7 @@ module ysyx_25040109_top (
 
   
      always @(*) begin
-        update_decode_state(pc,pc + 32'd4,next_pc,inst_ifu);
+        //update_decode_state(pc,pc + 32'd4,next_pc,inst_ifu);
     end
 
    
