a5d8b011441c95e54d0d5be617f6332ba93ed631
tracer-ysyx <tracer@ysyx.org>
2025-07-19 22:49:57 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:49:57 up  9:53,  5 users,  load average: 0.27, 0.23, 0.15

--- npc/include/cpu/difftest.h
+++ npc/include/cpu/difftest.h
@@ -8,7 +8,7 @@
 void difftest_skip_ref();
 void difftest_skip_dut(int nr_ref, int nr_dut);
 void difftest_set_patch(void (*fn)(void *arg), void *arg);
-void difftest_step(vaddr_t pc, vaddr_t npc);
+extern "C" void difftest_step(vaddr_t pc, vaddr_t npc);
 void difftest_detach();
 void difftest_attach();
 //extern "C" void init_difftest(char *ref_so_file, long img_size, int port);
