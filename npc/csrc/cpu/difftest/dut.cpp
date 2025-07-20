c645524626441587b9eb896b280d16f09505a45f
tracer-ysyx <tracer@ysyx.org>
2025-07-19 23:51:59 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:51:59 up 10:55,  5 users,  load average: 0.09, 0.10, 0.12

--- npc/csrc/cpu/difftest/dut.cpp
+++ npc/csrc/cpu/difftest/dut.cpp
@@ -58,11 +58,11 @@ void init_difftest(char *ref_so_file,long img_size,int port){
     void (*ref_difftest_init)(int) = reinterpret_cast<decltype(ref_difftest_init)>(dlsym(handle, "difftest_init"));
     assert(ref_difftest_init);
 
-    /*Log("Differential testing: %s", ANSI_FMT("ON", ANSI_FG_GREEN));
+    Log("Differential testing: %s", ANSI_FMT("ON", ANSI_FG_GREEN));
     Log("The result of every instruction will be compared with %s."
         "This will help you a lot for debugging, but also significantly reduce the performance ."
         "If it is not neccessary,you can turn it off in menuconfig .",
-        ref_so_file);*/
+        ref_so_file);
 
     ref_difftest_init(port);
     ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
