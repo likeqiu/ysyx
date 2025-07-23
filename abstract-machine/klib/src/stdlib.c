c5eb78c18e2ea4babf2681209d9b6ebd09c7fb7e
tracer-ysyx <tracer@ysyx.org>
2025-07-23 02:44:46 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:44:46 up 1 day, 19:31,  5 users,  load average: 0.06, 0.11, 0.08

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -2,6 +2,21 @@
 #include <klib.h>
 #include <klib-macros.h>
 
+
+
+
+static void *addr = NULL;
+
+static inline void *align_up(void *ptr,size_t align) {
+  uintptr_t p = (uintptr_t)ptr;
+  uintptr_t aligned = (p + align -1) & ~(align - 1);
+
+  return (void *)aligned;
+}
+
+
+
+
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
@@ -33,6 +48,26 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+
+  if (addr == NULL) {
+    addr = heap.start;
+  }
+
+  if (size == 0) {
+    return NULL;
+  }
+
+  void *current = align_up(addr, 8);
+
+  void *next_addr = (char *)current + size;
+
+  if (next_addr > heap.end) {
+    return NULL;
+  }
+
+  addr = next_addr;
+  return current;
+
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
   panic("Not implemented");
 #endif
