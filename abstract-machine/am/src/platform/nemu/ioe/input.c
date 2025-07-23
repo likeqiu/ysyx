2fc4ac1fc957e8f47ced47f0ea47c88948d47e7b
tracer-ysyx <tracer@ysyx.org>
2025-07-23 09:05:14 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:05:14 up 1 day, 23:56,  5 users,  load average: 0.24, 0.36, 0.21

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -8,7 +8,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint16_t kbd_data = inw(KBD_ADDR);
 
   uint8_t keycode = kbd_data & 0xff;
-  bool keydown = (kbd_data & 0x80000) != 0;
+  bool keydown = (kbd_data & 0x8000) != 0;
 
   kbd->keydown = keydown;
   kbd->keycode = keycode;
