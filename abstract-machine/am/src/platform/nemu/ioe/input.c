cd42743520688237198e607e9898c5e028584f7b
tracer-ysyx <tracer@ysyx.org>
2025-07-23 22:53:24 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:53:24 up 2 days,  3:39,  5 users,  load average: 0.01, 0.03, 0.04

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -8,14 +8,14 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 
   uint32_t scancode = inl(KBD_ADDR);
 
-  // 检查是否没有按键事件
-  if (scancode == AM_KEY_NONE) {
+
+  kbd->keydown = (scancode & KEYDOWN_MASK) != 0;
+
+
+  kbd->keycode = scancode & ~KEYDOWN_MASK;
+
+
+  if (kbd->keycode == AM_KEY_NONE) {
     kbd->keydown = false;
-    kbd->keycode = AM_KEY_NONE;
-  } else {
-
-    kbd->keydown = ((scancode & KEYDOWN_MASK) != 0);
-
-    kbd->keycode = scancode & ~KEYDOWN_MASK;
   }
 }
