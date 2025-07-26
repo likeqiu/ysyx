cc8094dabdd784e8a686422f8348e186d5813933
tracer-ysyx <tracer@ysyx.org>
2025-07-26 03:46:43 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:46:43 up 2 days,  4:08,  5 users,  load average: 0.23, 0.25, 0.43

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,14 +4,24 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+   int i;
+   uint32_t screen_info = inl(VGACTL_ADDR);
+   int w = screen_info >> 16;
+   int h = screen_info & 0xFFFF;
+   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+   for (i = 0; i < w * h; i ++) fb[i] = i;
+   outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+  uint32_t screen_info = inl(VGACTL_ADDR);
+  uint32_t width = screen_info >> 16;
+  uint32_t height = screen_info & 0xFFFF;
+  *cfg = (AM_GPU_CONFIG_T){.present = true,
+                           .has_accel = false,
+                           .width = width,
+                           .height = height,
+                           .vmemsz = width * height * sizeof(uint32_t)};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
