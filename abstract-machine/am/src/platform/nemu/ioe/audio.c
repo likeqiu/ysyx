a99441382b8a8c704ed857192783dc036bad7b2f
tracer-ysyx <tracer@ysyx.org>
2025-07-27 09:39:45 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:39:45 up 2 days, 18:34,  5 users,  load average: 0.25, 0.07, 0.05

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -2,6 +2,7 @@
 #include <nemu.h>
 #include<klib.h>
 
+static uint32_t SBUF_SIZE = 0;
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
 #define AUDIO_SAMPLES_ADDR   (AUDIO_ADDR + 0x08)
@@ -13,6 +14,7 @@ void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
+  SBUF_SIZE = inl(AUDIO_SBUF_SIZE_ADDR);
   cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
   cfg->present = true;
 }
@@ -46,23 +48,24 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   // 在循环前就打印一次状态
   printf("AM: Buffer check: sbuf_size=%u, count=%u, free_space=%d. Needed=%u\n",
          sbuf_size, count, sbuf_size - count, len);
-  while(sbuf_size - inl(AUDIO_COUNT_ADDR) < len){
-    //printf("AM: Waiting for buffer... Current count = %d\n",inl(AUDIO_COUNT_ADDR));
-    // 空间不足，CPU自旋等待
-    // 在真实操作系统中会使用更高效的等待方式（如让出CPU）
-    // 但在AM这个抽象层中，自旋是可接受的
-  }
+ 
 
   static uint32_t write_offset = 0;
   uint8_t *src = (uint8_t *)ctl->buf.start;
-
+  while (SBUF_SIZE - inl(AUDIO_COUNT_ADDR) < len) {
+    // printf("AM: Waiting for buffer... Current count =
+    // %d\n",inl(AUDIO_COUNT_ADDR));
+    //  空间不足，CPU自旋等待
+    //  在真实操作系统中会使用更高效的等待方式（如让出CPU）
+    //  但在AM这个抽象层中，自旋是可接受的
+  }
 
   for (int i = 0; i < len;i++){
-    uint32_t offset = (write_offset + i) % sbuf_size;
+    uint32_t offset = (write_offset + i) % SBUF_SIZE;
     outb(AUDIO_SBUF_ADDR + offset,src[i]);
   }
 
-    write_offset = (write_offset + len) % sbuf_size;
+  write_offset = (write_offset + len) % SBUF_SIZE;
 
   outl(AUDIO_COUNT_ADDR, len);
 
