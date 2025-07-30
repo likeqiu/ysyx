79301b808adcfbb169c6c1ce15eddac6b04060a1
tracer-ysyx <tracer@ysyx.org>
2025-07-30 05:35:11 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:35:11 up 3 days, 21:41,  5 users,  load average: 0.36, 0.23, 0.16

--- npc/include/cpu/difftest.h
+++ npc/include/cpu/difftest.h
@@ -6,21 +6,24 @@
 
 #ifdef CONFIG_DIFFTEST
 
-#ifdef __cplusplus
+
+#ifdef __caplusplus
 extern "C" {
+
+
 #endif
+    void difftest_skip_ref();
+    void difftest_skip_dut(int nr_ref, int nr_dut);
+    void difftest_set_patch(void (*fn)(void *arg), void *arg);
+    void difftest_step(vaddr_t pc, vaddr_t npc);
+    void difftest_detach();
+    void difftest_attach();
+    void init_difftest(char *ref_so_file, long img_size, int port);
 
-void difftest_skip_ref();
-void difftest_skip_dut(int nr_ref, int nr_dut);
-void difftest_set_patch(void (*fn)(void *arg), void *arg);
-void difftest_step(vaddr_t pc, vaddr_t npc);
-void difftest_detach();
-void difftest_attach();
-void init_difftest(char *ref_so_file, long img_size, int port);
-
-#ifdef __cplusplus
+#ifdef __caplusplus    
 }
 #endif
+
 
 /*extern "C" void difftest_skip_ref();
 extern "C" void difftest_skip_dut(int nr_ref, int nr_dut);
@@ -30,26 +33,15 @@ extern "C" void difftest_detach();
 extern "C" void difftest_attach();*/
 //extern "C" void init_difftest(char *ref_so_file, long img_size, int port);
 #else
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 static inline void difftest_skip_ref() {}
 static inline void difftest_skip_dut(int nr_ref, int nr_dut) {}
 static inline void difftest_set_patch(void (*fn)(void *arg), void *arg) {}
 static inline void difftest_step(vaddr_t pc, vaddr_t npc) {}
 static inline void difftest_detach() {}
 static inline void difftest_attach() {}
-static inline void init_difftest(char *ref_so_file, long img_size,
-                   int port); 
-
-#ifdef __cplusplus
-}
+static inline void init_difftest(char *ref_so_file, long img_size, int port);
 #endif
-
-#endif
-
+    
 extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction);
 extern void (*ref_difftest_regcpy)(void *dut, bool direction);
 extern void (*ref_difftest_exec)(uint64_t n);
