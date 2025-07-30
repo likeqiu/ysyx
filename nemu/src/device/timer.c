7ca03f2f70e75f57890acb481f8e2bd1f5433877
tracer-ysyx <tracer@ysyx.org>
2025-07-30 11:28:56 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:28:56 up 4 days,  1:58,  5 users,  load average: 0.11, 0.61, 0.67

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -24,17 +24,19 @@ static uint32_t *rtc_port_base = NULL;
 //    - len: 读写数据的长度（此实现中未使用）。
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
 
+  if (is_write)
+    return;
 
   //printf("111\n");
   
   assert(offset == 0 || offset == 4);
-  if (!is_write && offset == 4) {
+ // if (!is_write && offset == 4) {
     uint64_t us = get_time();
     rtc_port_base[0] = (uint32_t)us;
     // 将低32位存储到rtc_port_base[0]
     rtc_port_base[1] = us >> 32;
     // 将高32位存储到rtc_port_base[0]
-  }
+  //}
   // 行为: 当读取偏移量4时，调用get_time()获取当前时间，将64位微秒值拆分为
   //       两个32位寄存器（rtc_port_base[0] 和 rtc_port_base[1]）。
 }
