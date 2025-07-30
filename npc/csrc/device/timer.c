5458999f0d8bfcfa432bd61115cb8787a539ead4
tracer-ysyx <tracer@ysyx.org>
2025-07-30 03:53:58 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:53:58 up 3 days, 20:00,  5 users,  load average: 0.35, 0.34, 0.20

--- npc/csrc/device/timer.c
+++ npc/csrc/device/timer.c
@@ -20,14 +20,14 @@ static void clint_io_handler(uint32_t offset,int len,bool is_write){
 
 void init_timer(){
     uint8_t *space = new_space(0x4008);
-    
+
     add_mmio_map("timer", CONFIG_RTC_MMIO, space, 0x4008, clint_io_handler);
     
 }
 
-/*void clint_update_ctimecmp(uint64_t new_mtimecmp){
+void clint_update_ctimecmp(uint64_t new_mtimecmp){
     clint_mtimecmp = new_mtimecmp;
-}*/
+}
 
 uint64_t clint_get_mtime(){
     return clint_mtime;
