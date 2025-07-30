d06047a15bf5fbcd9767a82c52eacc62f23c2286
tracer-ysyx <tracer@ysyx.org>
2025-07-30 11:15:05 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:15:05 up 4 days,  1:44,  5 users,  load average: 0.75, 0.41, 0.45

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -81,12 +81,13 @@ void vga_update_screen() {
   }
 }
 
+/*
 static void vga_ctl_io_handler(uint32_t offset, int len, bool is_write) {
   // 我们只关心对 SYNC 寄存器（偏移量为4）的写操作
   if (offset == 4 && is_write) {
     vga_update_screen();
   }
-}
+}*/
 
 void init_vga() {
   vgactl_port_base = (uint32_t *)new_space(8);
@@ -96,7 +97,7 @@ void init_vga() {
   add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
 #else
   add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8,
-               vga_ctl_io_handler);
+               NULL);
 #endif
 
   vmem = new_space(screen_size());
