8c34fad02d82e56ac54fb4075a4b2b762ec64112
tracer-ysyx <tracer@ysyx.org>
2025-07-30 04:44:31 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:44:31 up 3 days, 20:50,  5 users,  load average: 0.20, 0.16, 0.11

--- npc/csrc/memory/paddr.c
+++ npc/csrc/memory/paddr.c
@@ -63,11 +63,11 @@ word_t paddr_read(paddr_t addr, int len)
 void paddr_write(paddr_t addr, int len, word_t data)
 {
 
-    if (likely(in_pmem(addr)))
-    {
-        pmem_write(addr, len, data);
-        return;
-    }
+  printf("222222\n");
+  if (likely(in_pmem(addr))) {
+    pmem_write(addr, len, data);
+    return;
+  }
     printf("222222\n");
     IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
     out_of_bound(addr);
