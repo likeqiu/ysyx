a54b29754f088ad7650b5dbd06f08a34b4e6040a
tracer-ysyx <tracer@ysyx.org>
2025-07-30 04:56:52 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:56:52 up 3 days, 21:02,  5 users,  load average: 0.04, 0.17, 0.14

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -76,7 +76,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
      mtrace_record('W', addr, len, data);
      return; }
 
-  printf("addr: 0x%08x\n",addr);
+  //printf("addr: 0x%08x\n",addr);
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); mtrace_record('W', addr, len, data); return);
   out_of_bound(addr);
 }
