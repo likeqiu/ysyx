f5261c8ea3ffb265c25da91c4e910447467a391b
tracer-ysyx <tracer@ysyx.org>
2025-07-30 05:30:23 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:30:23 up 3 days, 21:36,  5 users,  load average: 0.04, 0.10, 0.12

--- npc/include/cpu/difftest.h
+++ npc/include/cpu/difftest.h
@@ -19,7 +19,7 @@ void difftest_attach();
 void init_difftest(char *ref_so_file, long img_size, int port);
 
 #ifdef __cplusplus
-},
+}
 #endif
 
 /*extern "C" void difftest_skip_ref();
@@ -30,13 +30,21 @@ extern "C" void difftest_detach();
 extern "C" void difftest_attach();*/
 //extern "C" void init_difftest(char *ref_so_file, long img_size, int port);
 #else
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 static inline void difftest_skip_ref() {}
 static inline void difftest_skip_dut(int nr_ref, int nr_dut) {}
 static inline void difftest_set_patch(void (*fn)(void *arg), void *arg) {}
 static inline void difftest_step(vaddr_t pc, vaddr_t npc) {}
 static inline void difftest_detach() {}
 static inline void difftest_attach() {}
-void init_difftest(char *ref_so_file, long img_size, int port);
+void init_difftest(char *ref_so_file, long img_size,
+                   int port); 
+
+#ifdef __cplusplus
+}
 #endif
     
 extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction);
