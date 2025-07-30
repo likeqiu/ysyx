18cc49e4159455c00c3ff4402d9046715d80828e
tracer-ysyx <tracer@ysyx.org>
2025-07-30 03:10:01 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:10:01 up 3 days, 19:16,  5 users,  load average: 0.14, 0.32, 0.33

--- npc/csrc/device/io/map.c
+++ npc/csrc/device/io/map.c
@@ -54,7 +54,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;                  
   invoke_callback(map->callback, offset, len, false);
-  word_t ret = host_read(((uint8_t *))map->space + offset, len);
+  word_t ret = host_read((uint8_t *)map->space + offset, len);
   dtrace(map, 'R', ret);
   return ret;
 }
@@ -63,7 +63,7 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
-  host_write(((uint8_t *))map->space + offset, len, data);
+  host_write((uint8_t *)map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
   dtrace(map, 'W', data);
 }
