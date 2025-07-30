69252a1aab2d3652361dcc8b42c64f6cb2922fce
tracer-ysyx <tracer@ysyx.org>
2025-07-30 04:58:57 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:58:57 up 3 days, 21:05,  5 users,  load average: 0.02, 0.12, 0.12

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -11,7 +11,8 @@ extern char _pmem_start;
 Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
 
-void putch(char ch) {
+void putch(char ch) { 
+  outb(SERIAL_PORT, ch); 
 }
 
 void halt(int code) {
