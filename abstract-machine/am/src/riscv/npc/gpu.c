da4fed62091b5c86f4c1d9ccf6d18684ba3ed9d1
tracer-ysyx <tracer@ysyx.org>
2025-07-31 04:18:51 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:18:51 up  4:01,  1 user,  load average: 1.18, 0.80, 0.61

--- abstract-machine/am/src/riscv/npc/gpu.c
+++ abstract-machine/am/src/riscv/npc/gpu.c
@@ -4,14 +4,14 @@
 
 
 void __am_gpu_init() {
-  /*
+  
    int i;
    uint32_t screen_info = inl(VGACTL_ADDR);
    int w = screen_info >> 16;
    int h = screen_info & 0xFFFF;
    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
    for (i = 0; i < w * h; i ++) fb[i] = i;
-   outl(SYNC_ADDR, 1);*/
+   outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
