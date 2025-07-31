9a3572d810c8af8179e4eebc333365636a6c0077
tracer-ysyx <tracer@ysyx.org>
2025-07-31 03:37:11 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:37:11 up  3:19,  1 user,  load average: 0.03, 0.06, 0.12

--- abstract-machine/am/src/riscv/npc/serial.c
+++ abstract-machine/am/src/riscv/npc/serial.c
@@ -1,11 +1,14 @@
 #include<am.h>
 #include<npc.h>
+#include<klib.h>
 
 #define THR_REG (SERIAL_PORT + 0x0)
 #define LSR_REG (SERIAL_PORT + 0x5)
 #define LSR_THRE_BIT (1 << 5)
 
 void __am_uart_tx(AM_UART_TX_T *uart) {
+
+  printf("11111111\n");
   while ((*(volatile uint8_t *)LSR_REG & LSR_THRE_BIT) == 0)
     ;
 
@@ -13,5 +16,6 @@ void __am_uart_tx(AM_UART_TX_T *uart) {
 }
 
 void __am_uart_rx(AM_UART_RX_T *uart) {
-     uart->data = 0;
+  printf("2222222\n");
+  uart->data = 0;
 }
--- npc/csrc/device/device.c
+++ npc/csrc/device/device.c
@@ -4,15 +4,25 @@
 void init_map();
 void init_serial();
 void init_timer();
+void init_vga();
 
-void device_uodate()
-{
+#define TIMER_HZ 60
 
-    static uint64_t last = 0;
+    void device_uodate() {
+
+  static uint64_t last = 0; // 只初始化一次
+  uint64_t now = get_time();
+  if (now - last < 1000000 / TIMER_HZ) {
+    return; // 限制更新频率
+  }
+  last = now;
+
+  IFDEF(CONFIG_HAS_VGA, vga_update_screen());
 }
 
 void init_device(){
     init_map();
     IFDEF(CONFIG_HAS_SERIAL, init_serial());
     IFDEF(CONFIG_HAS_TIMER, init_timer());
+    IFDEF(CONFIG_HAS_VGA, init_vga());
 }
