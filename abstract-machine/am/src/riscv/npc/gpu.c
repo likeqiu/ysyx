7fc475f8c301cbc98a836b2d1617c745fe1b8b98
tracer-ysyx <tracer@ysyx.org>
2025-07-31 03:51:30 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:51:29 up  3:34,  1 user,  load average: 0.12, 0.07, 0.08

---
+++ abstract-machine/am/src/riscv/npc/gpu.c
@@ -0,0 +1,55 @@
+#include <am.h>
+#include <npc.h>
+
+#define SYNC_ADDR (VGACTL_ADDR + 4)
+
+void __am_gpu_init() {
+  /*
+   int i;
+   uint32_t screen_info = inl(VGACTL_ADDR);
+   int w = screen_info >> 16;
+   int h = screen_info & 0xFFFF;
+   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+   for (i = 0; i < w * h; i ++) fb[i] = i;
+   outl(SYNC_ADDR, 1);*/
+}
+
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+
+  uint32_t screen_info = inl(VGACTL_ADDR);
+  uint32_t width = screen_info >> 16;
+  uint32_t height = screen_info & 0xFFFF;
+  *cfg = (AM_GPU_CONFIG_T){.present = true,
+                           .has_accel = false,
+                           .width = width,
+                           .height = height,
+                           .vmemsz = width * height * sizeof(uint32_t)};
+}
+
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+
+  if (ctl->pixels != NULL && ctl->w != 0 && ctl->h != 0) {
+    uint32_t screen_width = inl(VGACTL_ADDR) >> 16;
+    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    uint32_t *pixels = ctl->pixels;
+    int x = ctl->x;
+    int y = ctl->y;
+    int w = ctl->w;
+    int h = ctl->h;
+
+    // 逐行复制像素
+    // 这一部分可以替换成memcpy
+    for (int j = 0; j < h; j++) {
+      for (int i = 0; i < w; i++) {
+
+        fb[(y + j) * screen_width + (x + i)] = pixels[j * w + i];
+      }
+    }
+  }
+
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
+  }
+}
+
+void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
