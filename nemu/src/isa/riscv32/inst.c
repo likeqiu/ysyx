093290793a48635da7aa73c3cd1b722e4f91a91f
tracer-ysyx <tracer@ysyx.org>
2025-08-01 02:09:42 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:09:42 up 15:00,  1 user,  load average: 0.00, 0.03, 0.06

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -224,7 +224,14 @@ static int decode_exec(Decode *s) {
   track_branch(s, taken, s->pc + imm, "bgeu");
 });
 
+  INSTPAT("??????? ????? ????? 001 ????? 1110011", csrw, I, {
+    cpu.csr[imm] = src1;
+  });
 
+  INSTPAT("000000000000 00000 000 00000 1110011", ecall, N, {
+    vaddr_t handler_addr = isa_raise_intr(11, s->pc);
+    s->dnpc = handler_addr;
+  });
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak, N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv, N, INV(s->pc));
 
