73451535ed23af3ad34b89bcfcac494e11fffbcf
tracer-ysyx <tracer@ysyx.org>
2025-07-31 23:20:27 -0400

    > run NEMU ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:20:27 up 13:51,  1 user,  load average: 0.12, 0.06, 0.02

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -19,12 +19,8 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-   
 
-  
-
-
-
+  cpu.csr[0] = 0;
 
   return 0;
 }
