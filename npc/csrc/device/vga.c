f5a5626d9fffc33d259bf9bd4c65cb0e98340b86
tracer-ysyx <tracer@ysyx.org>
2025-08-13 10:41:41 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:41:41 up 3 days,  4:41,  1 user,  load average: 0.67, 0.56, 0.35

--- npc/csrc/device/vga.c
+++ npc/csrc/device/vga.c
@@ -60,7 +60,7 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
-  printf("[NPC DEBUG] Sync bit FOUND! Updating screen...\n");
+
   if (vgactl_port_base[1]) {
     printf("[NPC DEBUG] Sync bit FOUND! Updating screen...\n");
     update_screen();
@@ -117,6 +117,7 @@ static void vga_blit_handler(uint32_t offset, int len, bool is_write) {
 
 // TILEBLIT 的处理函数：高性能的硬件加速器
 static void vga_tileblit_handler(uint32_t offset, int len, bool is_write) {
+  printf("[NPC DEBUG] TILEBLIT handler received request.\n");
   if (!is_write)
     return;
   printf("[NPC DEBUG] TILEBLIT handler received request.\n");
