c3fe20481ef496d08fa037caa8cc77063822449b
tracer-ysyx <tracer@ysyx.org>
2025-08-20 08:50:09 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:50:09 up 3 days, 15:32,  1 user,  load average: 0.32, 0.25, 0.19

--- npc/csrc/cpu/difftest/dut.cpp
+++ npc/csrc/cpu/difftest/dut.cpp
@@ -105,6 +105,7 @@ void difftest_step(vaddr_t pc,vaddr_t npc){
     }
     ref_difftest_exec(1);
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
+    printf("22222\n");
     checkregs(&ref_r, pc);
 }
 
