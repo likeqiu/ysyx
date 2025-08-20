f58ca7648f9c6ca7742518a0853034e85f57145d
tracer-ysyx <tracer@ysyx.org>
2025-08-20 09:09:24 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:09:24 up 3 days, 15:51,  1 user,  load average: 0.41, 0.38, 0.29

--- npc/obj_dir/Vysyx_25040109_top___024root__DepSet_hd5fae32a__0.cpp
+++ npc/obj_dir/Vysyx_25040109_top___024root__DepSet_hd5fae32a__0.cpp
@@ -63,7 +63,7 @@ VL_INLINE_OPT void Vysyx_25040109_top___024root___nba_sequent__TOP__0(Vysyx_2504
         Vysyx_25040109_top___024root____Vdpiimwrap_ysyx_25040109_top__DOT__itrace_print_TOP(vlSelf->pc, vlSelf->inst, 4U, vlSelf->p_count_number);
         Vysyx_25040109_top___024root____Vdpiimwrap_ysyx_25040109_top__DOT__printf_finish_TOP(vlSelf->inst, __Vfunc_ysyx_25040109_top__DOT__printf_finish__9__Vfuncout);
         if (VL_UNLIKELY((0U == __Vfunc_ysyx_25040109_top__DOT__printf_finish__9__Vfuncout))) {
-            VL_FINISH_MT("/home/zxj17/ysyx-workbench/npc/vsrc/ysyx_25040109_top.v", 304, "");
+            VL_FINISH_MT("/home/zxj17/ysyx-workbench/npc/vsrc/ysyx_25040109_top.v", 295, "");
         }
     }
     if (vlSelf->rst) {
