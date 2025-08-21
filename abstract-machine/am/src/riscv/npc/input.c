aa157e0ea68644db82e3bbe5e2005f36f7dc42fa
tracer-ysyx <tracer@ysyx.org>
2025-08-21 11:50:29 -0400

    > sim RTL ysyx_25040109 张熙健 Linux ZXJ17 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:50:29 up 4 days,  2:24,  1 user,  load average: 0.09, 0.05, 0.02

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -1,6 +1,17 @@
 #include <am.h>
+#include <npc.h>
+
+#define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+
+  uint32_t scancode = inl(KBD_ADDR);
+
+  kbd->keydown = (scancode & KEYDOWN_MASK) != 0;
+
+  kbd->keycode = scancode & ~KEYDOWN_MASK;
+
+  if (kbd->keycode == AM_KEY_NONE) {
+    kbd->keydown = false;
+  }
 }
